2da312aff09831d36d24fb49904b0fc1870f7dbe
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-01-16 13:06:15 +0100

    [FIX] sale_contract_specification: change model names in tests

--- sale_contract_specification/tests/test_sale_contract_specification.py
+++ sale_contract_specification/tests/test_sale_contract_specification.py
@@ -14,25 +14,34 @@ class TestSaleContractSpecification(common.SavepointCase):
         cls.sale_order = cls.env['sale.order'].create({
             'partner_id': cls.partner.id,
         })
-
-        cls.template = cls.env['sale.condition.template'].create({
+        cls.template1 = cls.env['contract.condition.template'].create({
             'name': 'Warranties',
         })
-        cls.condition = cls.env['sale.condition'].create({
+        cls.condition1 = cls.env['contract.condition'].create({
             'name': 'Warranty',
             'description': 'Products are guarantee by manufacturer',
-            'template_ids': [(4, cls.template.id)],
+            'template_ids': [(4, cls.template1.id)],
+        })
+        cls.template2 = cls.env['contract.condition.template'].create({
+            'name': 'Warranties',
+        })
+        cls.condition2 = cls.env['contract.condition'].create({
+            'name': 'Warranty',
+            'description': 'Products are guarantee by manufacturer',
+            'template_ids': [(4, cls.template2.id)],
         })
 
     def test_import(self):
         self.assertFalse(self.sale_order.condition_ids)
         self.assertFalse(self.sale_order.condition_tmpl_id)
-        self.sale_order.condition_tmpl_id = self.template
+        self.sale_order.condition_tmpl_id = self.template1
         self.assertFalse(self.sale_order.condition_ids)
         self.sale_order._onchange_condition_tmpl_id()
         self.assertTrue(self.sale_order.condition_ids)
         self.assertEqual(len(self.sale_order.condition_ids),
-                         len(self.template.condition_ids))
+                         len(self.template1.condition_ids))
+        self.sale_order.condition_tmpl_id = self.template2
         self.sale_order._onchange_condition_tmpl_id()
         self.assertEqual(len(self.sale_order.condition_ids),
-                         len(self.template.condition_ids) * 2)
+                         len(self.template1.condition_ids) +
+                         len(self.template2.condition_ids))
