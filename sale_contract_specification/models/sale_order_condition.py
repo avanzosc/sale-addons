89d76d74b0902841a4a942c1ad526f21db2a6a36
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-01-16 12:49:28 +0100

    [IMP] sale_contract_specification: conditions based on abstract

--- sale_contract_specification/models/sale_order_condition.py
+++ sale_contract_specification/models/sale_order_condition.py
@@ -6,17 +6,9 @@ from odoo import api, fields, models
 
 class SaleOrderCondition(models.Model):
     _name = 'sale.order.condition'
+    _inherit = ['order.condition']
     _description = 'Sale Order Condition'
     _order = 'condition_id, sale_id'
 
     sale_id = fields.Many2one(
         comodel_name='sale.order', string='Sale Order', required=True)
-    condition_id = fields.Many2one(
-        comodel_name='sale.condition', string='Condition')
-    description = fields.Text(string='Description', required=True)
-
-    @api.onchange('condition_id')
-    def _onchange_condition_id(self):
-        if self.condition_id:
-            self.description = (
-                self.condition_id.description or self.condition_id.name)
