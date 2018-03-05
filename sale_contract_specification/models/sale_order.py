645b7bae7c7bcd78b1fd868aa9f51e433148ffbc
oihane <oihanecruce@gmail.com>
2018-03-05 13:56:36 +0100

    [FIX] sale_contract_specification: execute condition_id onchange

--- sale_contract_specification/models/sale_order.py
+++ sale_contract_specification/models/sale_order.py
@@ -46,6 +46,7 @@ class SaleOrder(models.Model):
                         'description': x.description or x.name})
                 for x in self.draft_condition_tmpl_id.condition_ids]
             self.draft_condition_ids = condition_ids
+            self.draft_condition_ids._onchange_condition_id()
 
     @api.onchange('condition_tmpl_id')
     def _onchange_condition_tmpl_id(self):
@@ -59,3 +60,4 @@ class SaleOrder(models.Model):
                         'description': x.description or x.name})
                 for x in self.condition_tmpl_id.condition_ids]
             self.condition_ids = condition_ids
+            self.condition_ids._onchange_condition_id()
