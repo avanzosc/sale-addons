8ee488770475db1eaa4299611f8d1dff193bd08c
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-04-25 12:17:24 +0200

    [ADD] New module sale_school

---
+++ sale_school/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sales for School",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
