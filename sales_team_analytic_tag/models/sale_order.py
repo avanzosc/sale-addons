1b77af6a0dfa14c0ae50b40acbb3418d2d12b227
Daniel-CA <danielcampos@avanzosc.es>
2019-08-01 10:30:32 +0200

    [IMP] sales_team_analytic_tag:  Tags added on create function

--- sales_team_analytic_tag/models/sale_order.py
+++ sales_team_analytic_tag/models/sale_order.py
@@ -1,24 +1,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 # Copyright (c) 2019 Daniel Campos <danielcampos@avanzosc.es> - Avanzosc S.L.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    def _get_default_tag(self):
-        sale_obj = self.env['sale.order']
-        context_data = self.env.context.get('params', {})
-        tag_ids = {}
-        if not context_data:
-            tag_ids = self.env['crm.team']._get_default_team_id().\
-                analytic_tag_ids.ids
-        if 'model' in context_data and context_data['model'] == 'sale.order':
-            sale = sale_obj.browse(context_data['id'])
-            tag_ids = sale.team_id.analytic_tag_ids.ids
-        return tag_ids
-
     analytic_tag_ids = fields.Many2many(
         comodel_name='account.analytic.tag',
-        default=lambda self: self._get_default_tag())
+        )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super(SaleOrderLine, self).create(vals_list)
+        for line in lines:
+            line.analytic_tag_ids = list(
+                dict.fromkeys(line.analytic_tag_ids.ids +
+                              line.order_id.team_id.analytic_tag_ids.ids))
+        return lines
