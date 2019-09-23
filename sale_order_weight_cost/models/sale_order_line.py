24625e16ac6d910bc29bd04d3236dace53f695a7
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-09-23 14:46:07 +0200

    [IMP] sale_order_weight_cost: fix warning, digits_compute

--- sale_order_weight_cost/models/sale_order_line.py
+++ sale_order_weight_cost/models/sale_order_line.py
@@ -8,9 +8,16 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     weight = fields.Float(
-        string='Weight', digits_compute=dp.get_precision('Stock Weight'))
+        string='Weight', digits=dp.get_precision('Stock Weight'))
+    weight_uom_id = fields.Many2one(
+        comodel_name='uom.uom', string='Weight Unit of Measure',
+        compute='_compute_weight_uom_id')
+    weight_uom_name = fields.Char(
+        string='Weight unit of measure label', related='weight_uom_id.name',
+        readonly=True)
+    volume = fields.Float(string='Volume', help="The volume in m3.")
     cost = fields.Float(
-        string='Cost', digits_compute=dp.get_precision('Product Price'))
+        string='Cost', digits=dp.get_precision('Product Price'))
 
     @api.multi
     @api.onchange('product_id')
@@ -18,4 +25,12 @@ class SaleOrderLine(models.Model):
         res = super(SaleOrderLine, self).product_id_change()
         self.weight = self.product_id.weight
         self.cost = self.product_id.standard_price
+        self.volume = self.product_id.volume
         return res
+
+    @api.multi
+    def _compute_weight_uom_id(self):
+        tmpl_obj = self.env['product.template']
+        weight_uom_id = tmpl_obj._get_weight_uom_id_from_ir_config_parameter()
+        for line in self:
+            line.weight_uom_id = weight_uom_id
