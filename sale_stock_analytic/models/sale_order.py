064162c4633592c687eaaf96fe48cd14c1f97661
oihane <oihanecruce@gmail.com>
2019-10-17 15:00:12 +0200

    [IMP] sale_stock_analytic: erase duplications (#63)

--- sale_stock_analytic/models/sale_order.py
+++ sale_stock_analytic/models/sale_order.py
@@ -9,17 +9,8 @@ class SaleOrder(models.Model):
     @api.multi
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
-        for sale in self.filtered(lambda c: not c.analytic_account_id):
-            account = self.env['account.analytic.account'].create(
-                sale._catch_vals_for_analytic())
-            sale.analytic_account_id = account.id
         for sale in self.filtered(lambda c: c.analytic_account_id):
-            for picking in sale.mapped('picking_ids').filtered(
-                    lambda x: not x.analytic_account_id):
-                picking.analytic_account_id = sale.analytic_account_id.id
+            sale.mapped('picking_ids').write({
+                'analytic_account_id': sale.analytic_account_id.id,
+            })
         return res
-
-    def _catch_vals_for_analytic(self):
-        vals = {'name': self.name,
-                'partner_id': self.partner_id.id}
-        return vals
--- sale_stock_analytic/tests/test_sale_stock_analytic.py
+++ sale_stock_analytic/tests/test_sale_stock_analytic.py
@@ -1,29 +1,33 @@
 # Copyright 2019 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo.tests.common import TransactionCase
+from odoo.tests import common
 
 
-class TestSaleStockAnalytic(TransactionCase):
+@common.at_install(False)
+@common.post_install(True)
+class TestSaleStockAnalytic(common.SavepointCase):
 
-    def setUp(self):
-        super(TestSaleStockAnalytic, self).setUp()
-        self.product = self.env.ref('product.consu_delivery_01')
+    @classmethod
+    def setUpClass(cls):
+        super(TestSaleStockAnalytic, cls).setUpClass()
+        cls.env.user.company_id.autocreate_sale_analytic_account = True
+        cls.product = cls.env.ref('product.consu_delivery_01')
         sale_vals = {
             'name': 'Sale for test sale_stock_analytic',
-            'partner_id': self.env.ref('base.res_partner_12').id}
+            'partner_id': cls.env.ref('base.res_partner_12').id}
         line_vals = {
-            'product_id': self.product.id,
-            'name': self.product.name,
+            'product_id': cls.product.id,
+            'name': cls.product.name,
             'product_uom_qty': 1,
-            'product_uom': self.product.uom_id.id,
+            'product_uom': cls.product.uom_id.id,
             'price_unit': 100}
         sale_vals['order_line'] = [(0, 0, line_vals)]
-        self.sale = self.env['sale.order'].create(sale_vals)
+        cls.sale = cls.env['sale.order'].create(sale_vals)
 
     def test_sale_stock_analytic(self):
+        self.assertFalse(self.sale.analytic_account_id)
         self.sale.action_confirm()
-        self.assertNotEqual(
-            self.sale.analytic_account_id, False)
+        self.assertTrue(self.sale.analytic_account_id)
         self.assertEqual(
             self.sale.analytic_account_id,
             self.sale.picking_ids[0].analytic_account_id)
