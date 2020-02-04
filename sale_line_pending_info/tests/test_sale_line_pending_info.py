a212f466f3f71ec657d9c56389f327bae1a068a6
Thor <alfredodelafuente@avanzosc.es>
2020-02-04 09:03:28 +0100

    [ADD] sale_line_pending_info: In sale lines new fields: Pending delivery qty, amount pending delivery, pending invoicing qty, amount pending invoicing.

---
+++ sale_line_pending_info/tests/test_sale_line_pending_info.py
@@ -0,0 +1,33 @@
+# Copyright 2019 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo.tests import common
+
+
+@common.at_install(False)
+@common.post_install(True)
+class TestSaleLinePendingInfo(common.SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestSaleLinePendingInfo, cls).setUpClass()
+        cls.sale_model = cls.env['sale.order']
+        cls.report_model = cls.env['sale.report']
+
+    def test_sale_line_pending_info(self):
+        cond = [('state', 'in', ('sale', 'done'))]
+        sale = self.sale_model.search(cond, limit=1)
+        for line in sale.order_line:
+            self.assertEquals(line.qty_pending_delivery,
+                              line.product_uom_qty - line.qty_delivered)
+            self.assertEquals(line.amount_pending_delivery,
+                              line.qty_pending_delivery * line.price_unit)
+            self.assertEquals(line.qty_pending_invoicing,
+                              line.product_uom_qty - line.qty_invoiced)
+            self.assertEquals(line.amount_pending_invoicing,
+                              line.qty_pending_invoicing * line.price_unit)
+        res = self.report_model._query(
+            with_clause='', fields=None, groupby='', from_clause='')
+        self.assertIn('as qty_pending_delivery', res)
+        self.assertIn('as qty_pending_invoicing', res)
+        self.assertIn('as amount_pending_delivery', res)
+        self.assertIn('as amount_pending_invoicing', res)
