5fa1ea495d3dc34cb44385de45300f752cb8dfe1
oihane <oihanecruce@gmail.com>
2020-03-13 09:49:29 +0100

    [IMP] sale_school: filter lines by school (#85)

--- sale_school/models/sale_order_template_option.py
+++ sale_school/models/sale_order_template_option.py
@@ -1,11 +1,11 @@
-# Copyright 2019 Oihane Crucelaegui - AvanzOSC
+# Copyright 2020 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
 
-class SaleOrderTemplateLine(models.Model):
-    _inherit = "sale.order.template.line"
+class SaleOrderTemplateOption(models.Model):
+    _inherit = "sale.order.template.option"
 
     company_id = fields.Many2one(
         string="Company", comodel_name="res.company",
