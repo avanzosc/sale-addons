b5ec07eeba1d7a5509624a84efca4bf5ed6e9032
Thor <alfredodelafuente@avanzosc.es>
2020-03-26 13:52:10 +0100

    [ADD] sale_report_pivot_ext: In "Sale report pivot" new field "Commitment date".

---
+++ sale_report_pivot_ext/report/sale_report.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class SaleReport(models.Model):
+    _inherit = 'sale.report'
+
+    commitment_date = fields.Date(
+        string='Commitment Date', readonly=True)
+
+    def _query(self, with_clause='', fields=None, groupby='', from_clause=''):
+        if not fields:
+            fields = {}
+        fields['commitment_date'] = (
+            ', s.commitment_date_without_hour as commitment_date')
+        groupby += ', s.commitment_date_without_hour'
+        return super(SaleReport, self)._query(
+            with_clause=with_clause, fields=fields, groupby=groupby,
+            from_clause=from_clause)
