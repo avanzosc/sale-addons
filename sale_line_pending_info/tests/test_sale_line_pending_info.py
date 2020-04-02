5630bbb964d0bb2db41a888b811e4f57cb7c2e10
Thor <alfredodelafuente@avanzosc.es>
2020-04-02 12:08:44 +0200

    [IMP] sale_line_pending_info: Qty and amound totals in sale order.

--- sale_line_pending_info/tests/test_sale_line_pending_info.py
+++ sale_line_pending_info/tests/test_sale_line_pending_info.py
@@ -16,15 +16,32 @@ class TestSaleLinePendingInfo(common.SavepointCase):
     def test_sale_line_pending_info(self):
         cond = [('state', 'in', ('sale', 'done'))]
         sale = self.sale_model.search(cond, limit=1)
+        sale.order_line.write({'discount': 5})
         for line in sale.order_line:
             self.assertEquals(line.qty_pending_delivery,
                               line.product_uom_qty - line.qty_delivered)
-            self.assertEquals(line.amount_pending_delivery,
-                              line.qty_pending_delivery * line.price_unit)
+            amount = line.qty_pending_delivery * line.price_unit
+            if line.discount:
+                amount -= (amount * line.discount) / 100
+            self.assertEquals(line.amount_pending_delivery, amount)
             self.assertEquals(line.qty_pending_invoicing,
                               line.product_uom_qty - line.qty_invoiced)
-            self.assertEquals(line.amount_pending_invoicing,
-                              line.qty_pending_invoicing * line.price_unit)
+            amount = line.qty_pending_invoicing * line.price_unit
+            if line.discount:
+                amount -= (amount * line.discount) / 100
+            self.assertEquals(line.amount_pending_invoicing, amount)
+        self.assertEquals(
+            sale.total_qty_pending_delivery,
+            sum(sale.order_line.mapped('qty_pending_delivery')))
+        self.assertEquals(
+            sale.total_amount_pending_delivery,
+            sum(sale.order_line.mapped('amount_pending_delivery')))
+        self.assertEquals(
+            sale.total_qty_pending_invoicing,
+            sum(sale.order_line.mapped('qty_pending_invoicing')))
+        self.assertEquals(
+            sale.total_amount_pending_invoicing,
+            sum(sale.order_line.mapped('amount_pending_invoicing')))
         res = self.report_model._query(
             with_clause='', fields=None, groupby='', from_clause='')
         self.assertIn('as qty_pending_delivery', res)
