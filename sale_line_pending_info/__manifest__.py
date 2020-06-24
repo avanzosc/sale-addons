b15bda0eb704de4b2c49c327e5f2a58637685403
Thor <alfredodelafuente@avanzosc.es>
2020-06-24 14:48:34 +0200

    [IMP] sale_line_pending_info: Change calculation of field "qty_pending_delivery".

--- sale_line_pending_info/__manifest__.py
+++ sale_line_pending_info/__manifest__.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Line Pending Info",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Sales",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
     "depends": [
         "sale_order_line_input",
+        "sale_stock"
     ],
     "data": [
         "views/sale_order_view.xml",
