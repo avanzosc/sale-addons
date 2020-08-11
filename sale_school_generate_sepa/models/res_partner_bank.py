334eb0f4bd60f98a451aa983e17cfd8cd9fc9ba6
Oihane Crucelaegui <oihanecruce@gmail.com>
2020-08-11 11:21:04 +0200

    [IMP] sale_school_generate_sepa: new find mandate function

---
+++ sale_school_generate_sepa/models/res_partner_bank.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ResPartnerBank(models.Model):
+    _inherit = "res.partner.bank"
+
+    @api.multi
+    def _find_mandate(self, originator):
+        self.ensure_one()
+        bank_mandates = (
+            self.mandate_ids.sorted("signature_date", reverse=True)
+                .filtered(
+                lambda m: m.company_id == originator))
+        mandates = bank_mandates.filtered(lambda m: m.state == "valid")
+        if not mandates:
+            mandates = bank_mandates.filtered(lambda m: m.state == "draft")
+        return mandates and mandates[:1]
