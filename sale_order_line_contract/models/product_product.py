49520642311e305b99de7e9f400d317b23b9fe85
Thor <alfredodelafuente@avanzosc.es>
2021-05-11 07:57:34 +0200

    [ADD] sale_order_line_contract: Create contract line where confirm one sale order.

---
+++ sale_order_line_contract/models/product_product.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, fields, _
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    recurring_interval = fields.Integer(
+        string='Invoice Every', default=1)
+    recurring_rule_type = fields.Selection(
+        [('daily', _('Day(s)')),
+         ('weekly', _('Week(s)')),
+         ('monthly', _('Month(s)')),
+         ('monthlylastday', _('Month(s) last day')),
+         ('quarterly', _('Quarter(s)')),
+         ('semesterly', _('Semester(s)')),
+         ('yearly', _('Year(s)'))],
+        string='Recurrence')
