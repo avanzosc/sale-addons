910d9a7ae1b74e42562ddafc3a9a771efd703605
Avanzosc <leiremartinez@avanzosc.es>
2021-07-08 08:54:14 +0200

    [IMP] sale_create_mandate: make changes on code

--- sale_create_mandate/models/__init__.py
+++ sale_create_mandate/models/__init__.py
@@ -1,3 +1,4 @@
 
 from . import res_partner
+from . import res_partner_bank
 from . import sale_order
--- sale_create_mandate/models/res_partner.py
+++ sale_create_mandate/models/res_partner.py
@@ -14,8 +14,8 @@ class ResPartner(models.Model):
     def _compute_partner_validate_bank_account(self):
         for record in self:
             record.error_bank_acc = False
-            if not record.bank_ids or True in record.bank_ids.mapped(
-                    'error_bank_acc'):
+            if not record.bank_ids or any(record.mapped(
+                    "bank_ids.error_bank_acc")):
                 record.error_bank_acc = True
 
     def create_validate_bank_account_mandate(self):
@@ -32,31 +32,3 @@ class ResPartner(models.Model):
                         bank_id.create_validate_bank_account_mandate()
         else:
             raise ValidationError("The customer does not have a bank account.")
-
-
-class ResPartnerBank(models.Model):
-    _inherit = 'res.partner.bank'
-
-    error_bank_acc = fields.Boolean(
-        string='Bank account error',
-        compute="_compute_validate_bank_account")
-
-    @api.depends('acc_type')
-    def _compute_validate_bank_account(self):
-        for record in self:
-            record.error_bank_acc = False
-            if record.acc_type not in 'iban':
-                record.error_bank_acc = True
-
-    def create_validate_bank_account_mandate(self):
-        wiz_obj = self.sudo().env["res.partner.bank.mandate.generator"]
-        mandate_wiz = wiz_obj.create({
-            "bank_ids": [self.id],
-            "mandate_format": "sepa",
-            "mandate_type": "recurrent",
-            "mandate_scheme": "CORE",
-            "mandate_recurrent_sequence_type": "recurring",
-            "signed": True,
-            "validate": True,
-        })
-        mandate_wiz.button_generate_mandates()
