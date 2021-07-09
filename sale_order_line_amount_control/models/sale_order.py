2400d040de2fcf8338eb04343dac6416fc61fcf3
oihane <oihanecruce@gmail.com>
2021-07-09 09:05:33 +0200

    [IMP] sale_school: if future student has sale order but no child take it from the order (#166)

--- sale_order_line_amount_control/models/sale_order.py
+++ sale_order_line_amount_control/models/sale_order.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import models, api, _
 from odoo.exceptions import UserError
-from openerp.tools import config
+from odoo.tools import config
 
 
 class SaleOrder(models.Model):
