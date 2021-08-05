2bd55452646276197a70777dc93769f9a8ef78ca
leiremartinezdesantos <72156354+leiremartinezdesantos@users.noreply.github.com>
2021-08-05 07:44:15 +0200

    [ADD] sale_create_mandate: create mandate on SO confirm (#163)
    * [ADD] sale_create_mandate: create mandate on SO confirm

---
+++ sale_create_mandate/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Sale Create Mandate",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'version': '14.0.1.0.0',
+    'depends': [
+        'account_banking_create_mandate',
+        'account_payment_sale',
+        'sale'
+    ],
+    'data': [
+        'views/views.xml'
+    ]
+}
