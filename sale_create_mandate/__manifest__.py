19b75b0ef3597d5d03ed59b463c9c3701bc5eb8f
oihane <oihanecruce@gmail.com>
2021-08-18 12:01:37 +0200

    [IMP] sale_create_mandate: fix dependencies (#170)

--- sale_create_mandate/__manifest__.py
+++ sale_create_mandate/__manifest__.py
@@ -1,17 +1,18 @@
 # Copyright 2021 Leire Martinez de Santos - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Sale Create Mandate",
+    "name": "Sale Create Mandate",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
-    'version': '14.0.1.0.0',
-    'depends': [
-        'account_banking_create_mandate',
-        'account_payment_sale',
-        'sale'
+    "version": "14.0.1.0.0",
+    "depends": [
+        "account_banking_mandate_usability",
+        "account_payment_sale",
+        "sale",
     ],
-    'data': [
-        'views/views.xml'
-    ]
+    "data": [
+        "views/views.xml",
+    ],
+    "installable": True,
 }
