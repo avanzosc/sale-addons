edeca962e5891ec9d868c69bc80d029edf97a45b
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-09-01 09:17:59 +0200

    [ADD] sale_order_line_attachment: Adjuntos en líneas de pedido de venta. (#175)

---
+++ sale_order_line_attachment/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Attachment",
+    "version": "12.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "mail"
+    ],
+    "category": "Sales",
+    "data": [
+        "views/sale_order_views.xml",
+        "views/ir_attachment_views.xml",
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
