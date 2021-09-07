bd6dfae26198166c7fa1075dfdc0800310db7f9e
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-09-07 09:54:19 +0200

    [ADD] sale_order_line_manual_sequence: In "sale order line", "stock move", and "stock move line" objects, new field "Manual seq.". (#176)

---
+++ sale_order_line_manual_sequence/models/account_move.py
@@ -0,0 +1,56 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields, api
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    manual_sequence = fields.Char(
+        string='Manual seq.')
+
+    @api.model
+    def _move_autocomplete_invoice_lines_create(self, vals_list):
+        new_vals_list = super(
+            AccountMove, self)._move_autocomplete_invoice_lines_create(
+                vals_list)
+        for old in vals_list:
+            data = False
+            if 'invoice_line_ids' in old:
+                for line in old.get('invoice_line_ids'):
+                    if (len(line) == 3 and line[0] == 0 and not
+                        str(line[1]).isdigit() and
+                        'virtual' in line[1] and
+                            'manual_sequence' in line[2]):
+                        data = line
+                        for new in new_vals_list:
+                            if 'line_ids' in new:
+                                for line in new.get('line_ids'):
+                                    if (len(line) == 3 and line[0] == 0 and not
+                                        str(data[1]).isdigit() and
+                                        'virtual' in line[1] and
+                                            line[1] == data[1]):
+                                        line[2]['manual_sequence'] = (
+                                            data[2].get(
+                                                'manual_sequence', ''))
+        return new_vals_list
+
+    def _move_autocomplete_invoice_lines_write(self, vals):
+        if 'invoice_line_ids' in vals:
+            for line in vals.get('invoice_line_ids'):
+                if (len(line) == 3 and line[0] == 0 and not
+                    str(line[1]).isdigit() and
+                    'virtual' in line[1] and
+                        'manual_sequence' in line[2]):
+                    data = line
+                    if 'line_ids' in vals:
+                        for line in vals.get('line_ids'):
+                            if (len(line) == 3 and line[0] == 0 and not
+                                str(data[1]).isdigit() and
+                                'virtual' in line[1] and
+                                    line[1] == data[1]):
+                                line[2]['manual_sequence'] = (
+                                    data[2].get(
+                                        'manual_sequence', ''))
+        return super(
+            AccountMove, self)._move_autocomplete_invoice_lines_write(vals)
---
+++ sale_order_line_manual_sequence/models/account_move_line.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    manual_sequence = fields.Char(
+        string='Manual seq.')
