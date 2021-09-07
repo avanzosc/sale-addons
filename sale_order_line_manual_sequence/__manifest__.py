0ade42976d651dba4df1355275b4d7cbcae8e27e
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-09-07 09:54:19 +0200

    [ADD] sale_order_line_manual_sequence: In "sale order line", "stock move", and "stock move line" objects, new field "Manual seq.". (#176)

---
+++ sale_order_line_manual_sequence/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Line Manual Sequence",
+    "version": "13.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+        "stock",
+        "account"
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/stock_picking_view.xml",
+        "views/account_move_view.xml",
+    ],
+    "installable": True,
+}
