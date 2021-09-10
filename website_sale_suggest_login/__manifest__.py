0f298ac33f3c0048ea7e66d5942f40e571e9b671
Avanzosc <leiremartinez@avanzosc.es>
2021-09-10 11:32:42 +0200

    [ADD] website_sale_suggest_login: choose website checkout redirect

---
+++ website_sale_suggest_login/__manifest__.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Leire Martinez de Santos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Website Sale Suggest Login",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    'version': '14.0.1.0.0',
+    'depends': ['website_sale_suggest_create_account'],
+    'data': ['views/templates.xml', 'views/views.xml'],
+}
