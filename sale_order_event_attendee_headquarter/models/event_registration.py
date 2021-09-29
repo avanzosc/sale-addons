9c7c30df2b50ab64f04fff7c16427a3e5bba2df3
Thor <alfredodelafuente@avanzosc.es>
2021-09-29 09:05:08 +0200

    [ADD] sale_order_event_attendee_headquarter: When creating a sales order by confirming an event registration, bring the event "headquarter" to the created sales order.

---
+++ sale_order_event_attendee_headquarter/models/event_registration.py
@@ -0,0 +1,16 @@
+from odoo import models
+
+
+class EventRegistration(models.Model):
+    _inherit = 'event.registration'
+
+    def write(self, values):
+        result = super(EventRegistration, self).write(values)
+        if 'sale_order_id' in values and values.get('sale_order_id', False):
+            sale_order = self.env['sale.order'].browse(
+                values.get('sale_order_id'))
+            for registration in self.filtered(
+                    lambda x: x.event_id and x.event_id.organizer_id):
+                sale_order.headquarter_id = (
+                    registration.event_id.organizer_id.id)
+        return result
---
+++ sale_order_event_attendee_headquarter/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_sale_order_event_attendee_headquarter
