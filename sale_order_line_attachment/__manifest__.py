a3b544495ade1b8ec19374b640f6c569a6b76cdb
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2021-10-05 15:20:58 +0200

    [IMP] sale_order_line_attachment: New wizard for select product attachments. (#186)

--- sale_order_line_attachment/__manifest__.py
+++ sale_order_line_attachment/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Sale Order Line Attachment",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
     "license": "AGPL-3",
@@ -12,6 +12,7 @@
     ],
     "category": "Sales",
     "data": [
+        "wizard/wiz_catch_attachment_from_product_view.xml",
         "views/sale_order_views.xml",
         "views/ir_attachment_views.xml",
         "views/product_template_views.xml",
