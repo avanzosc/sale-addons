86320290384a6d16d32f5ee1bdb65ae1e16add21
Tu Nombre <you@example.com>
2021-10-21 09:54:18 +0200

    [ADD] sale_order_product_template: Product template field in sale report pivot view.

---
+++ sale_report_product_template/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Berezi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Report Product Template",
+    'version': '12.0.1.0.0',
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Sale",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_report_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
