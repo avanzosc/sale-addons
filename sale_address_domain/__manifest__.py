796583bca89ef7f5ead70a35ff4ade9e9b6b04b8
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-01-27 16:31:13 +0100

    [MIG] sale_address_domain: Migration to 14.0

--- sale_address_domain/__manifest__.py
+++ sale_address_domain/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Sale Address Domain",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "sale",
     ],
     "author": "AvanzOSC",
-    "website": "https://github.com/OCA/l10n-spain",
+    "website": "http://www.avanzosc.es",
     "category": "Sales",
     "data": [
         "views/sale_order_view.xml",
