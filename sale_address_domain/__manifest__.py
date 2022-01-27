424188417059c6f6ae20eaefe71734ddfbb0fcdf
oihane <oihanecruce@gmail.com>
2022-01-27 17:22:37 +0100

    [MIG] sale_address_domain: Migration to 14.0 (#197)
    * [ADD] sale_order_directions_domain: In sales orders let only select shipping and billing addresses.
    
    * [IMP] sale_address_domain: black, isort, prettier
    
    * [MIG] sale_address_domain: Migration to 14.0
    
    Co-authored-by: Thor <alfredodelafuente@avanzosc.es>

---
+++ sale_address_domain/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Address Domain",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Sales",
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
