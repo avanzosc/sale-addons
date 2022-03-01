9e542c71fffb133295f53576cdeb69b8ed197ba2
Thor <alfredodelafuente@avanzosc.es>
2022-03-01 15:29:54 +0100

    [ADD] sale_project_usability: In task form, display the product and quantity of the sale line.

---
+++ sale_project_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Project Usability",
+    "version": "14.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale_project",
+    ],
+    "data": [
+        "views/project_task_view.xml",
+    ],
+    "installable": True,
+}
