5f0d1ea753463f0e670d5573dddae74538d94027
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-03-18 08:55:50 +0100

    [ADD] sale_project_usability: In task form, display the product and quantity of the sale line. (#205)

---
+++ sale_project_usability/models/project_task.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    sale_line_product_id = fields.Many2one(
+        string='Sale line product', comodel_name='product.product',
+        related='sale_line_id.product_id', store=True)
+    sale_line_product_uom_qty = fields.Float(
+        string='Sale line quantity', digits='Product Unit of Measure',
+        related='sale_line_id.product_uom_qty', store=True)
