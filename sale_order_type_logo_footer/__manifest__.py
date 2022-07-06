60db264118caf2440a5b391c40d51ae773353f6f
Thor <alfredodelafuente@avanzosc.es>
2022-07-06 13:21:45 +0200

    [ADD] sale_order_type_logo_footer: In "Sale order types" new fields: "Logo for reports", and "Footer for reports".

---
+++ sale_order_type_logo_footer/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Type Logo Footer",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale_order_type",
+    ],
+    "excludes": [],
+    "data": [
+        "views/sale_order_type_view.xml",
+    ],
+    "installable": True,
+}
