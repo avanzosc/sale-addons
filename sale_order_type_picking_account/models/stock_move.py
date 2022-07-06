d580a5de1f5c25c3cc2759c3400b734d1f93e5df
Thor <alfredodelafuente@avanzosc.es>
2022-07-06 13:13:47 +0200

    [ADD] sale_order_type_picking_account: In picking new field "Sale type".

---
+++ sale_order_type_picking_account/models/stock_move.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _get_new_picking_values(self):
+        values = super(StockMove, self)._get_new_picking_values()
+        group = self.mapped('group_id')
+        if group and group.sale_id and group.sale_id.type_id:
+            values['sale_type_id'] = group.sale_id.type_id.id
+        return values
