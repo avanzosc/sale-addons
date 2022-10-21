3e73acdd91468e7ca73ab24700b13a0a6542d809
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-10-21 08:35:12 +0200

    [ADD] sale_order_type_logo_footer: In "Sale order types" new fields: "Logo for reports", and "Footer for reports". (#219)

---
+++ sale_order_type_logo_footer/models/sale_order_type.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleOrderType(models.Model):
+    _inherit = "sale.order.type"
+
+    logo_for_reports = fields.Binary(
+        "Logo for reports")
+    footer_for_reports = fields.Text(
+        "Footer for reports", translate=True)
