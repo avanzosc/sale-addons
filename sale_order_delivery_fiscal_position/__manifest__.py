2b4bdd7f6c1de3cdf647dbfcf594257639e7ad1f
Thor <alfredodelafuente@avanzosc.es>
2022-11-02 14:27:43 +0100

    [ADD] sale_order_delivery_fiscal_position: Make the "Fiscal position" field visible in "Shipping address" type contacts.

---
+++ sale_order_delivery_fiscal_position/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Delivery Fiscal Position",
+    "version": "14.0.1.0.0",
+    "category": "Sales/Sales",
+    "author":  "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/sale-addons.git",
+    "depends": [
+        "account",
+        "sale",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+        ],
+    "installable": True,
+    "auto_install": False,
+}
