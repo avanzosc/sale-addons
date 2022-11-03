d2c66ec9ea0bebbeb01ea0b24acd230d54b78dd2
Thor <alfredodelafuente@avanzosc.es>
2022-11-03 09:10:10 +0100

    [IMP] sale_stock_picking_analytic: Take analytical account of the sales order, to the out picking.

---
+++ sale_stock_picking_analytic/models/sale_order.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_confirm(self):
+        result = super(SaleOrder, self).action_confirm()
+        for sale in self.filtered(lambda x: x.analytic_account_id):
+            pickings = sale.pickings.filtered(
+                lambda z: not z.analytic_account_id)
+            if pickings:
+                pickings.write(
+                    {'analytic_account_id': sale.analytic_account_id})
+        return result
