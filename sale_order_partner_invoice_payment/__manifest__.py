a4b003aae5f4daf55967a727635cc74e2690cca0
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-11-23 08:47:56 +0100

    [ADD] sale_order_partner_invoice_payment: When selecting invoice address in sales orders, bring the mode, and the term of payment of the invoice address. (#212)

---
+++ sale_order_partner_invoice_payment/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Partner Invoice Payment",
+    'version': '14.0.1.0.0',
+    "category": "Banking addons",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+        "account_payment_sale",
+    ],
+    "data": [
+    ],
+    'installable': True,
+}
