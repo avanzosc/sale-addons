b4cadf2b5a778600ca02d7771da91610cf9e32b9
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-12-27 16:45:11 +0100

    [ADD] sale_order_line_last_price_info: In sale order line new fields: "Sale Last Unit Price", and "Invoice Last Unit Price". (#236)

---
+++ sale_order_line_last_price_info/models/account_move_line.py
@@ -0,0 +1,11 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    invoice_date = fields.Date(
+        string="Invoice/Bill Date", related="move_id.invoice_date", store=True,
+        copy=False)
