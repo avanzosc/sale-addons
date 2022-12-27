7985fabad70b594245a8738cf6fcbda652ef5749
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-12-27 16:45:11 +0100

    [ADD] sale_order_line_last_price_info: In sale order line new fields: "Sale Last Unit Price", and "Invoice Last Unit Price". (#236)

---
+++ sale_order_line_last_price_info/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Last Price Info",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "https://github.com/avanzosc/sale-addons",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+        "account"
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
