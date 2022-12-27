34b605b9598619a829c5643f85444b3a4b8bff95
Thor <alfredodelafuente@avanzosc.es>
2022-12-27 11:32:53 +0100

    [ADD] sale_order_line_qty_by_packaging: On sales order lines, if "Packaging Quantity" is entered, calculate the "Quantity".

---
+++ sale_order_line_qty_by_packaging/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Qty By Packaging",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "https://github.com/avanzosc/sale-addons",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
