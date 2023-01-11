acffd2dfb10cabb0d87f80641e4a8bbf8a2ed53d
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-01-11 10:23:04 +0100

    [ADD] New module sale_goal

---
+++ sale_goal/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Goals",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "excludes": [],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/res_users_views.xml",
+    ],
+    "installable": True,
+}
