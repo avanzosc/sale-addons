0ebb4684f780c6b8d4a54e52db8d27c33a56fc3e
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-01-12 14:29:18 +0100

    [IMP] sale_goal: remove store & translations * remove sale_goal_report

--- sale_goal/models/res_users.py
+++ sale_goal/models/res_users.py
@@ -24,27 +24,54 @@ class ResUsers(models.Model):
     sales_goal_monthly = fields.Float(
         string="Monthly Sales Goal",
         compute="_compute_sales_goal",
-        compute_sudo=True,
-        store=True,
     )
     sales_goal_yearly = fields.Float(
         string="Yearly Sales Goal",
         compute="_compute_sales_goal",
-        compute_sudo=True,
-        store=True,
     )
-    current_month_sale_amount = fields.Float()
-    current_year_sale_amount = fields.Float()
+    current_month_sale_amount = fields.Float(
+        compute="_compute_current_month_sale_amount",
+        string="Current Month Sale Amount",
+    )
+    current_year_sale_amount = fields.Float(
+        compute="_compute_current_year_sale_amount",
+        string="Current Year Sale Amount",
+    )
 
     def _compute_sale_partner_count(self):
         for record in self:
             record.sale_partner_count = len(record.sale_partner_ids)
 
-    @api.depends("sale_partner_ids",
-                 "sale_partner_ids.sales_goal_monthly",
-                 "sale_partner_ids.sales_goal_yearly")
+    @api.depends(
+        "sale_partner_ids",
+        "sale_partner_ids.sales_goal_monthly",
+        "sale_partner_ids.sales_goal_yearly",
+    )
     def _compute_sales_goal(self):
         for record in self:
             partners = record.sale_partner_ids
             record.sales_goal_monthly = sum(partners.mapped("sales_goal_monthly"))
             record.sales_goal_yearly = sum(partners.mapped("sales_goal_yearly"))
+
+    def _compute_current_month_sale_amount(self):
+        today = fields.Date.context_today(self)
+        for record in self:
+            sale_orders = record.commercial_sale_order_ids.filtered(
+                lambda o: today.month == o.date_order.month
+                and today.year == o.date_order.year
+                and o.state not in ("draft", "sent", "cancel")
+            )
+            record.current_month_sale_amount = sum(
+                sale_orders.mapped("order_line.price_subtotal")
+            )
+
+    def _compute_current_year_sale_amount(self):
+        today = fields.Date.context_today(self)
+        for record in self:
+            sale_orders = record.commercial_sale_order_ids.filtered(
+                lambda o: today.year == o.date_order.year
+                and o.state not in ("draft", "sent", "cancel")
+            )
+            record.current_year_sale_amount = sum(
+                sale_orders.mapped("order_line.price_subtotal")
+            )
--- sale_goal/models/sale_order_line.py
+++ sale_goal/models/sale_order_line.py
@@ -4,31 +4,45 @@ from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     sales_goal_yearly_percentage = fields.Float(
-        'Yearly objective',
+        string="Yearly Goal Percentage",
         compute="_compute_sales_goal_yearly_percentage",
         default=0.0,
-        store=True,)
+        store=True,
+    )
     sales_goal_monthly_percentage = fields.Float(
-        'Monthly objective', 
-        copmute="_compute_sales_goal_monthly_percentage",
+        string="Monthly Goal Percentage",
+        compute="_compute_sales_goal_monthly_percentage",
         default=0.0,
-        store=True,)
+        store=True,
+    )
 
-    @api.depends('order_partner_id', 'order_partner_id.sales_goal_monthly', 'price_subtotal')
+    @api.depends(
+        "order_partner_id", "order_partner_id.sales_goal_monthly", "price_subtotal"
+    )
     def _compute_sales_goal_monthly_percentage(self):
         for record in self:
             if record.order_partner_id.sales_goal_monthly:
-                record.sales_goal_monthly_percentage = record.price_subtotal * 100 / record.order_partner_id.sales_goal_monthly
+                record.sales_goal_monthly_percentage = (
+                    record.price_subtotal
+                    * 100
+                    / record.order_partner_id.sales_goal_monthly
+                )
             else:
                 record.sales_goal_monthly_percentage = 0.0
 
-    @api.depends('order_partner_id', 'order_partner_id.sales_goal_yearly', 'price_subtotal')
+    @api.depends(
+        "order_partner_id", "order_partner_id.sales_goal_yearly", "price_subtotal"
+    )
     def _compute_sales_goal_yearly_percentage(self):
         for record in self:
             if record.order_partner_id.sales_goal_yearly:
-                record.sales_goal_yearly_percentage = record.price_subtotal * 100 / record.order_partner_id.sales_goal_yearly
+                record.sales_goal_yearly_percentage = (
+                    record.price_subtotal
+                    * 100
+                    / record.order_partner_id.sales_goal_yearly
+                )
             else:
                 record.sales_goal_yearly_percentage = 0.0
