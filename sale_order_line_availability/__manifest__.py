02c6b8781673ceeade6d118c6139b3bb70eea502
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-01-26 12:07:04 +0100

    [ADD] sale_order_line_availability: In sale order lines new fields: Real quantity(RQ), and available quantity(AQ). (#247)

---
+++ sale_order_line_availability/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2023 Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Line Availability",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "sale",
+        "stock",
+        "sale_stock"
+    ],
+    "author":  "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "data": [
+        "views/sale_order_line_view.xml",
+        "views/sale_order_view.xml",
+        ],
+    "installable": True,
+}
