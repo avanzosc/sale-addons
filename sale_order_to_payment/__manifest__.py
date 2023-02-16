fafe4d17730b1214dd415a19358a5690ef6f237c
Tu Nombre <you@example.com>
2023-02-16 15:43:25 +0100

    [14.0][ADD] sale_order_to_payment: button in sale orders to payments.

---
+++ sale_order_to_payment/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order To Payment",
+    'version': '14.0.1.0.0',
+    "category": "Analytic",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "account",
+        "sale",
+        "account_move_to_payment",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    'installable': True,
+}
