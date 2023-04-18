ad6a618d4099855be89fb6f4177a27fb373a2288
Tu Nombre <you@example.com>
2023-04-18 15:08:12 +0200

    [14.0][ADD] sale_order_line_commitment_date:

---
+++ sale_order_line_commitment_date/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Line Commitment Date",
+    "version": "14.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+        "sale_stock",
+        "sale_order_line_input",
+        "sale_order_type",
+        "sale_line_pending_info",
+        "sale_order_line_menu",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/sale_order_line_view.xml",
+    ],
+    "installable": True,
+}
