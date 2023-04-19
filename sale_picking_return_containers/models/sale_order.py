609993a3204df6392f0e98afea36ef936175d132
Tu Nombre <you@example.com>
2023-04-19 11:59:00 +0200

    [14.0][ADD] sale_picking_return_containers:

---
+++ sale_picking_return_containers/models/sale_order.py
@@ -0,0 +1,38 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def action_create_return_purchase_order(self):
+        self.ensure_one()
+        if not self.partner_id:
+            raise UserError(_("The sale order does not have the partner."))
+        if not self.type_id:
+            raise UserError(_("The sale order does not have the type."))
+        if not self.type_id.picking_type_id:
+            raise UserError(
+                _("The sales order type does not have the picking type."))
+        if self.type_id.picking_type_id.code != "outgoing":
+            raise UserError(_("The picking type must be outgoing."))
+        if not self.type_id.picking_type_id.return_picking_type_id:
+            raise UserError(_(
+                "The picking type must have informed the return picking type.")
+            )
+        purchase = self.env["purchase.order"].create({
+            "partner_id": self.partner_id.id,
+            "picking_type_id": (
+                self.type_id.picking_type_id.return_picking_type_id.id),
+            "is_devolution": True})
+        purchase.action_return_returnable()
+        return {
+            "name": _("Return Purchase Order"),
+            "view_mode": "form,tree",
+            "res_model": "purchase.order",
+            "res_id": purchase.id,
+            "type": "ir.actions.act_window",
+            "context": dict(self.env.context),
+        }
---
+++ sale_picking_return_containers/models/sale_order_line.py
@@ -0,0 +1,26 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    returned_amount = fields.Float(
+        string="Returned Amount")
+    pending_qty = fields.Float(
+        string="Pending Qty")
+
+    @api.onchange("returned_amount", "qty_delivered")
+    def onchange_pending_qty(self):
+        if self.returned_amount or self.qty_delivered:
+            self.pending_qty = self.qty_delivered - self.returned_amount
+
+    @api.constrains("price_unit", "pending_qty")
+    def _check_pending_qty(self):
+        for line in self:
+            if line.pending_qty < 0 and line.price_unit != 0:
+                raise ValidationError(
+                    _("The pending quantity negative only can be if " +
+                      "the price unit is 0."))
