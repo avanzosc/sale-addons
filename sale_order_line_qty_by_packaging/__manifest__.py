288f2ecd5f3d43bf77a194ac9713bcc4f3268498
Tu Nombre <you@example.com>
2023-05-04 08:28:20 +0200

    [14.0][ADD] sale_order_line_qty_by_packaging:

---
+++ sale_order_line_qty_by_packaging/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Qty By Packaging",
+    "version": "14.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
