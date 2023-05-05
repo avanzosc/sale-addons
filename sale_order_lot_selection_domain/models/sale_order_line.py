42349bb2e00b162bd9f2de78b3f82542702863e5
Tu Nombre <you@example.com>
2023-05-05 10:08:28 +0200

    [14.0][IMP] sale_order_lot_selection_domain:

--- sale_order_lot_selection_domain/models/sale_order_line.py
+++ sale_order_lot_selection_domain/models/sale_order_line.py
@@ -6,6 +6,11 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    tracking = fields.Selection(
+        string="Tracking",
+        related="product_id.tracking",
+        store=True)
+
     @api.onchange("product_id")
     def onchange_lot_domain(self):
         self.ensure_one()
@@ -15,9 +20,20 @@ class SaleOrderLine(models.Model):
             lot = self.env["stock.production.lot"].search([
                 ('product_id', '=', self.product_id.id),
                 ("company_id", "=", self.company_id.id)])
-            for line in lot:
-                qty = line.product_qty
-                if qty > 0:
-                    lots.append(line.id)
+            if self.order_id.type_id and (
+                self.order_id.type_id.picking_type_id) and (
+                    self.order_id.type_id.picking_type_id.default_location_src_id):
+                quant = self.env["stock.quant"].search([
+                    ("product_id", "=", self.product_id.id),
+                    ("company_id", "=", self.company_id.id),
+                    ("location_id", "=", self.order_id.type_id.picking_type_id.default_location_src_id.id),
+                    ("inventory_quantity", ">", 0)])
+                for line in quant:
+                    lots.append(line.lot_id.id)
+            else:
+                for line in lot:
+                    qty = line.product_qty
+                    if qty > 0:
+                        lots.append(line.id)
             result = {"domain": {"lot_id": [('id', 'in', lots)]}}
         return result
