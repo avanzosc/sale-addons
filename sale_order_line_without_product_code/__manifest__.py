0797df58655dbba9c1648fbe8258b703caf8c23f
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-06-01 08:24:54 +0200

    [ADD] sale_order_line_without_product_code: Sale order line description without product internal reference. (#269)

---
+++ sale_order_line_without_product_code/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Without Product Code",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
---
+++ sale_order_line_without_product_code/models/sale_order_line.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.onchange('product_id')
+    def product_id_change(self):
+        return super(SaleOrderLine, self.with_context(
+            display_default_code=False)).product_id_change()
