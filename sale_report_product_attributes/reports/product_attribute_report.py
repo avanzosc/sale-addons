33114ab034a23de4beec875f68175a10956b1306
Leire <leiremartinez@avanzosc.es>
2023-06-14 16:12:41 +0200

    TRY WITH LESS INFO

--- sale_report_product_attributes/reports/product_attribute_report.py
+++ sale_report_product_attributes/reports/product_attribute_report.py
@@ -9,7 +9,7 @@ class ProductAttributeSaleReport(models.Model):
     _description = 'Product Attribute Sale Report'
     _auto = False
     _rec_name = "order_line_id"
-    _order = "order_line_id,product_id,attribute_id,attribute_value_id"
+    _order = "order_line_id,product_id,attribute_id" #,attribute_value_id"
 
     order_line_id = fields.Many2one(
         'sale.order.line', 'Order Line')
@@ -17,8 +17,8 @@ class ProductAttributeSaleReport(models.Model):
         comodel_name="product.product", string="Product")
     attribute_id = fields.Many2one(
         'product.attribute', 'Product Attribute')
-    attribute_value_id = fields.Many2one(
-        'product.attribute.value', 'Attribute Value')
+    # attribute_value_id = fields.Many2one(
+    #     'product.attribute.value', 'Attribute Value')
     product_uom_qty = fields.Float('Ordered Qty')
 
     def _select(self):
@@ -28,7 +28,6 @@ class ProductAttributeSaleReport(models.Model):
                     ol.id AS order_line_id,
                     p.id AS product_id,
                     ptal.attribute_id AS attribute_id,
-                    ptav.product_attribute_value_id AS attribute_value_id,
                     ol.product_uom_qty AS product_uom_qty
             """
         return select_str
@@ -40,14 +39,13 @@ class ProductAttributeSaleReport(models.Model):
                     JOIN product_template as pt ON pt.id = p.product_tmpl_id
                     JOIN product_template_attribute_line ptal ON ptal.product_tmpl_id = pt.id
                     JOIN product_attribute att ON ptal.attribute_id = att.id
-                    JOIN product_attribute_value attv ON attv.attribute_id = att.id
-                    JOIN product_template_attribute_value ptav ON attv.id = ptav.product_attribute_value_id 
+             
             """
         return from_str
 
     def _group_by(self):
         group_by_str = """
-                GROUP BY ol.id, p.id, ptal.attribute_id, ptav.product_attribute_value_id
+                GROUP BY ol.id, p.id, ptal.attribute_id
              """
         return group_by_str
 
