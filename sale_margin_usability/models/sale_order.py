0083178893b00d29e2e8c40105a8ad459a62fd46
Alfredo <alfredodelafuente@avanzosc.es>
2023-07-20 06:56:02 +0200

    [ADD] sale_margin_usability: Mostrar el total coste de las líneas en los pedidos de venta, y en la vista tree de los pedidos de venta.

---
+++ sale_margin_usability/models/sale_order.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    total_cost = fields.Float(
+        string="Total Cost", compute="_compute_total_cost",
+        digits="Product Price", store=True, readonly=False,
+        groups="base.group_user")
+
+    @api.depends("order_line", "order_line.total_cost")
+    def _compute_total_cost(self):
+        for sale in self:
+            sale.total_cost = sum(sale.order_line.mapped("total_cost"))
