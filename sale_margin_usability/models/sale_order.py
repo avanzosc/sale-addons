1b56e77eb84feb8aa3273ccce5686d47ee1604ba
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-07-21 13:08:00 +0200

    [IMP] sale_margin_usability: fields is monetary

--- sale_margin_usability/models/sale_order.py
+++ sale_margin_usability/models/sale_order.py
@@ -6,10 +6,8 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    total_cost = fields.Float(
-        string="Total Cost", compute="_compute_total_cost",
-        digits="Product Price", store=True, readonly=False,
-        groups="base.group_user")
+    total_cost = fields.Monetary(
+        string="Total Cost", compute="_compute_total_cost", store=True, readonly=False, groups="base.group_user")
 
     @api.depends("order_line", "order_line.total_cost")
     def _compute_total_cost(self):
