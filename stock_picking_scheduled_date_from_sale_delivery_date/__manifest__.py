54d5a7b283c1308a182ce7ee131756cba2db29dc
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-08-30 08:35:16 +0200

    [ADD] stock_picking_schedule_date_from_sale_delivery_date: When modifying the delivery date in the sale order, the scheduled date of the picking is also modified. (#290)

---
+++ stock_picking_scheduled_date_from_sale_delivery_date/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Scheduled Date From Sale Delivery Date",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "sale",
+        "stock",
+    ],
+    "excludes": [],
+    "data": [
+    ],
+    "installable": True,
+}
