d19f50fd9e1172993d28b7a2fcf3ffb287c3a5a6
oihane <oihanecruce@gmail.com>
2023-09-12 14:38:43 +0200

    [IMP] sale_order_line_last_price_info: pre-commit stuff (#296)

--- sale_order_line_last_price_info/__manifest__.py
+++ sale_order_line_last_price_info/__manifest__.py
@@ -5,12 +5,9 @@
     "version": "16.0.1.0.0",
     "category": "Sales/Sales",
     "license": "AGPL-3",
-    "author": "https://github.com/avanzosc/sale-addons",
-    "website": "http://www.avanzosc.es",
-    "depends": [
-        "sale",
-        "account"
-    ],
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": ["sale", "account"],
     "data": [
         "views/sale_order_views.xml",
     ],
