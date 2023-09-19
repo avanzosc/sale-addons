06129edfc50008a93e0b63072abaaa62b19f4471
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-09-19 11:30:07 +0200

    [IMP] sale_order_line_qty_by_packaging: Traer empaquetado del producto automáticamente a la línea de venta. (#295)

--- sale_order_line_qty_by_packaging/models/sale_order_line.py
+++ sale_order_line_qty_by_packaging/models/sale_order_line.py
@@ -10,6 +10,13 @@ class SaleOrderLine(models.Model):
     product_packaging_id = fields.Many2one(compute=False, precompute=False)
     product_packaging_qty = fields.Float(compute=False, precompute=False)
 
+    @api.onchange('product_id')
+    def _onchange_product_id_warning(self):
+        result = super(SaleOrderLine, self)._onchange_product_id_warning()
+        if self.product_id and len(self.product_id.packaging_ids) == 1:
+            self.product_packaging_id = self.product_id.packaging_ids[0].id
+        return result
+
     @api.onchange("product_packaging_id")
     def _onchange_product_packaging_id(self):
         if self.product_packaging_id:
