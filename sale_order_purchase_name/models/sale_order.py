2ab077eb95c8eced1fa3477f156aadc5108260fe
Alfredo <alfredodelafuente@avanzosc.es>
2023-09-20 09:50:00 +0200

    [ADD] sale_order_purchase_name: In sale order new field "Purchae orders.

---
+++ sale_order_purchase_name/models/sale_order.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    purchase_name = fields.Char(
+        string="Purchase Orders",
+        compute="_compute_purchase_name",
+        compute_sudo=True)
+
+    def _compute_purchase_name(self):
+        for sale in self:
+            purchase_name = ""
+            if sale.purchase_order_count > 0:
+                purchases = sale._get_purchase_orders()
+                for purchase in purchases:
+                    purchase_name = (
+                        purchase.name if not purchase_name else
+                        "{}, {}".format(purchase_name, purchase.name))
+            sale.purchase_name = purchase_name
