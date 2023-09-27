d43517040e8aac1c1aca7f7c2801946bb0350030
Alfredo <alfredodelafuente@avanzosc.es>
2023-09-27 14:28:44 +0200

    [ADD] sale_computer_manager: In sale order linew new fields "RAM" and "Storage 1 Size".

---
+++ sale_computer_management/models/stock_move.py
@@ -0,0 +1,54 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    ram_id = fields.Many2one(
+        string="RAM", comodel_name="ram", related="sale_line_id.ram_id",
+        copy=False, store=True,
+    )
+    storage1_size_id = fields.Many2one(
+        string="Storage 1 Size", comodel_name="storage.size",
+        related="sale_line_id.storage1_size_id", copy=False, store=True)
+    grade_id = fields.Many2one(
+        string="Grade", comodel_name="grade", copy=False, store=True,
+        related="sale_line_id.grade_id")
+    grade_tested = fields.Selection(
+        string="Grade tested", related="sale_line_id.grade_tested", store=True,
+        copy=False)
+    allowed_lots_ids = fields.Many2many(
+        string="Allowed lots", comodel_name="stock.production.lot",
+        compute="_compute_allowed_lots")
+
+    def _update_reserved_quantity(self, need, available_quantity, location_id,
+                                  lot_id=None, package_id=None, owner_id=None,
+                                  strict=True):
+        self.ensure_one()
+        return super(
+            StockMove, self.with_context(
+                origin_move=self))._update_reserved_quantity(
+                    need, available_quantity, location_id, lot_id=lot_id,
+                    package_id=package_id, owner_id=owner_id, strict=strict)
+
+    def _get_available_quantity(self, location_id, lot_id=None,
+                                package_id=None, owner_id=None, strict=False,
+                                allow_negative=False):
+        self.ensure_one()
+        return super(
+            StockMove, self.with_context(
+                origin_move=self))._get_available_quantity(
+                    location_id, lot_id=lot_id, package_id=package_id,
+                    owner_id=owner_id, strict=strict,
+                    allow_negative=allow_negative)
+
+    def _compute_allowed_lots(self):
+        for move in self:
+            lots = False
+            quants = self.env["stock.quant"].with_context(
+                origin_move=move)._gather(move.product_id, move.location_id)
+            if quants:
+                lots = quants.mapped("lot_id")
+            move.allowed_lots_ids = [(6, 0, lots.ids)] if lots else []
---
+++ sale_computer_management/models/stock_quant.py
@@ -0,0 +1,46 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    ram_id = fields.Many2one(
+        string="RAM", comodel_name="ram", related="lot_id.ram_id",
+        copy=False, store=True,)
+    storage1_size_id = fields.Many2one(
+        string="Storage 1 Size", comodel_name="storage.size",
+        related="lot_id.storage1_size_id", copy=False, store=True)
+    grade_id = fields.Many2one(
+        string="Grade", comodel_name="grade", copy=False, store=True,
+        related="lot_id.grade_id")
+    grade_tested = fields.Selection(
+        string="Grade tested", related="lot_id.grade_tested", store=True,
+        copy=False)
+
+    def _gather(self, product_id, location_id, lot_id=None, package_id=None,
+                owner_id=None, strict=False):
+        quants = super(StockQuant, self)._gather(
+            product_id, location_id, lot_id=lot_id, package_id=package_id,
+            owner_id=owner_id, strict=strict)
+        if "origin_move" not in self.env.context:
+            return quants
+        if quants:
+            quants = quants.filtered(
+                lambda x: x.grade_tested and x.grade_tested == "ok")
+        move = self.env.context.get("origin_move")
+        if not move.ram_id and not move.storage1_size_id and not move.grade_id:
+            return quants
+        if quants and move.ram_id:
+            quants = quants.filtered(
+                lambda x: x.ram_id and x.ram_id == move.ram_id)
+        if quants and move.storage1_size_id:
+            quants = quants.filtered(
+                lambda x: x.storage1_size_id and
+                x.storage1_size_id == move.storage1_size_id)
+        if quants and move.grade_id:
+            quants = quants.filtered(
+                lambda x: x.grade_id and x.grade_tested and
+                x.grade_id == move.grade_id and x.grade_tested == "ok")
+        return quants
