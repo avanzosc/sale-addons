a847328e9cf9aaa27d3b1a6fe018b4f93696e2f9
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-10-10 14:01:58 +0200

    [ADD] sale_order_purchase_name: In sale order new field "Purchae orders. (#302)

---
+++ sale_order_purchase_name/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Purchase Name",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale_purchase",
+    ],
+    "excludes": [],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
