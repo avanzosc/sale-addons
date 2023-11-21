5d1dd7a06e6ae1d9223c04c94e7f067d6740815e
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-11-21 09:30:29 +0100

    [IMP] sale_import_wizard: Campos a importar editables + onchages depués crear pedido venta. (#311)

--- sale_import_wizard/__manifest__.py
+++ sale_import_wizard/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Import Wizard",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Hidden/Tools",
     "license": "AGPL-3",
     "author": "AvanzOSC",
