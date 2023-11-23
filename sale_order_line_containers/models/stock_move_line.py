c59fa447560cf2ce6f46808d931eaca23c02592d
Tu Nombre <you@example.com>
2023-11-23 08:36:41 +0100

    [14.0][ADD] sale_order_line_containers:

---
+++ sale_order_line_containers/models/stock_move_line.py
@@ -0,0 +1,17 @@
+# © 2022 Berezi Amubieta - AvanzOSC
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    container = fields.Integer(
+        string="Container")
+
+    @api.onchange("move_id")
+    def onchange_move_id(self):
+        self.ensure_one()
+        if self.move_id:
+            self.container = self.move_id.container
