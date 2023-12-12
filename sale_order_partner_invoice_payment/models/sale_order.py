edf9137a3363af54476a635ef2aeb893ebbdc9b3
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-12-12 16:32:24 +0100

    [IMP] sale_order_partner_invoice_payment: field is computed

--- sale_order_partner_invoice_payment/models/sale_order.py
+++ sale_order_partner_invoice_payment/models/sale_order.py
@@ -1,27 +1,16 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, api
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    @api.onchange("partner_invoice_id")
-    def onchange_partner_invoice_id(self):
-        for sale in self:
-            if (sale.partner_id and sale.partner_invoice_id and
-                sale.partner_id != sale.partner_invoice_id and
-                    sale.partner_invoice_id.property_payment_term_id):
-                sale.payment_term_id = (
-                    sale.partner_invoice_id.property_payment_term_id.id)
+    _inherit = "sale.order"
 
     @api.depends("partner_id", "partner_invoice_id")
     def _compute_payment_mode(self):
-        result = super(SaleOrder, self)._compute_payment_mode()
-        for sale in self:
-            if (sale.partner_id and sale.partner_invoice_id and
-                sale.partner_id != sale.partner_invoice_id and
-                    sale.partner_invoice_id.customer_payment_mode_id):
-                sale.payment_mode_id = (
-                    sale.partner_invoice_id.customer_payment_mode_id.id)
-        return result
+        super()._compute_payment_mode()
+        for order in self.filtered(
+            lambda sale: sale.partner_invoice_id
+            and sale.partner_invoice_id.customer_payment_mode_id
+        ):
+            order.payment_mode_id = order.partner_invoice_id.customer_payment_mode_id
