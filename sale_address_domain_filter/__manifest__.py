657d255998b93bcbbbc432295c7ff92ab1e2070e
Tu Nombre <you@example.com>
2023-12-19 11:10:47 +0100

    [14.0][ADD] sale_address_domain_filter:

---
+++ sale_address_domain_filter/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Address Domain Filter",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Sales",
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
