a168396981bb6fc9c93cc6742f1fe75a640f2ef6
Unai Beristain <unaiberis@gmail.com>
2023-12-26 15:00:05 +0100

    [MIG] sale_order_vehicle: Migration to 16.0

--- sale_order_vehicle/__manifest__.py
+++ sale_order_vehicle/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Vehicle",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/sale-addons",
     "category": "Sales/Sales",
