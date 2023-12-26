553a748d020081361bb3d9e192dac852a0c20f98
Unai Beristain <unaiberis@gmail.com>
2023-12-26 15:24:11 +0100

    [MIG] sale_stock_picking_analytic: Migration to 16.0

--- sale_stock_picking_analytic/__manifest__.py
+++ sale_stock_picking_analytic/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sales and Warehouse Management - Analytic",
     "summary": "Glue module between sale_stock and stock_picking_analytic",
-    "version": "14.0.1.2.0",
+    "version": "16.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
     "author": "AvanzOSC",
