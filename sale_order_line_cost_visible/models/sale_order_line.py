b3583224a19219d99f23f55dee1a0cb6d97aa296
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-12-27 11:51:12 +0100

    [ADD] sale_order_line_cost_visible: Show "Cost" field in sale order line, if the user is in the group "Visible costs in products". (#308)

---
+++ sale_order_line_cost_visible/models/sale_order_line.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    purchase_price = fields.Float(
+        groups="product_cost_visible.group_product_cost_visible")
