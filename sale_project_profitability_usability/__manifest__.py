593ad898ef1d3e989f9d5f5cb4ef74792b11839c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-12-29 10:05:44 +0100

    [ADD] sale_project_profitability_usability: In project new fields "Total out invoice with tax", "Amount collected",  "Total in invoice with tax", "Amount paid". (#341)

---
+++ sale_project_profitability_usability/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Project Profitability Usability" ,
+    "version": "16.0.1.0.0",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "project_profitability_usability",
+        "sale_project"
+        ],
+    "data": [
+        "views/project_project_views.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+}
