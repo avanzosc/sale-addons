bbab0181e2c2a05bc73fefd2f2482cbfd7c60e28
Unai Beristain <unaiberistain@avanzosc.es>
2024-01-02 14:55:18 +0100

    [IMP] sale_partner_company: pre-commit stuff

--- sale_partner_company/__manifest__.py
+++ sale_partner_company/__manifest__.py
@@ -4,14 +4,10 @@
 {
     "name": "Sale Partner Company",
     "version": "14.0.1.0.0",
-    "depends": [
-        "sale",
-        "contacts",
-        "account"
-    ],
-    "author":  "AvanzOSC",
+    "depends": ["sale", "contacts", "account"],
+    "author": "AvanzOSC",
     "license": "AGPL-3",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/sale-addons",
     "data": [
         "views/sale_partner_company_view.xml",
     ],
