cb0c81eaf340369eec846b26cb7a01bc95fcd6b8
oihane <oihanecruce@gmail.com>
2024-01-05 08:34:38 +0100

    [IMP] sale_project_profitability_usability: pre-commit stuff (#344)

--- sale_project_profitability_usability/__manifest__.py
+++ sale_project_profitability_usability/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Sale Project Profitability Usability" ,
+    "name": "Sale Project Profitability Usability",
     "version": "16.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
@@ -9,11 +9,10 @@
     "website": "https://github.com/avanzosc/sale-addons",
     "depends": [
         "project_profitability_usability",
-        "sale_project"
-        ],
+        "sale_project",
+    ],
     "data": [
-        "views/project_project_views.xml"
+        "views/project_project_views.xml",
     ],
     "installable": True,
-    "auto_install": True,
 }
