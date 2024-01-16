ec413ca4ea30673c201f26cc949d13afef91cc91
Alfredo <alfredodelafuente@avanzosc.es>
2024-01-16 08:15:05 +0100

    [FIX] sale_order_type_picking: Fix error in sale order when change sale order type.

--- sale_order_type_picking/models/sale_order.py
+++ sale_order_type_picking/models/sale_order.py
@@ -1,15 +1,22 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.onchange("type_id")
-    def onchange_type_id(self):
-        result = super().onchange_type_id()
-        for order in self:
-            if order.type_id and order.type_id.carrier_id:
-                order.carrier_id = order.type_id.carrier_id.id
-        return result
+    carrier_id = fields.Many2one(
+        compute="_compute_carrier_id", store=True, readonly=False
+    )
+
+    @api.depends("type_id")
+    def _compute_carrier_id(self):
+        res = None
+        if hasattr(super(), "_compute_carrier_id"):
+            res = super()._compute_carrier_id()
+        for order in self.filtered("type_id"):
+            order_type = order.type_id
+            if order_type.carrier_id:
+                order.carrier_id = order_type.carrier_id.id
+        return res
