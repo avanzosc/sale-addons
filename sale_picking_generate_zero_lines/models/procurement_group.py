4d7a5ac2f5e633cb46e2c17e22b8b323fa4188b4
Tu Nombre <you@example.com>
2024-01-26 13:18:58 +0100

    [14.0][ADD] sale_picking_generate_zero_lines: It generate lines on the picking even if they quantity is zero.

---
+++ sale_picking_generate_zero_lines/models/procurement_group.py
@@ -0,0 +1,63 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import logging
+from collections import defaultdict
+from odoo.exceptions import UserError
+
+
+from odoo import api, fields, models
+from odoo.tools import float_is_zero
+
+_logger = logging.getLogger(__name__)
+
+class ProcurementException(Exception):
+    """An exception raised by ProcurementGroup `run` containing all the faulty
+    procurements.
+    """
+    def __init__(self, procurement_exceptions):
+        """:param procurement_exceptions: a list of tuples containing the faulty
+        procurement and their error messages
+        :type procurement_exceptions: list
+        """
+        self.procurement_exceptions = procurement_exceptions
+
+class ProcurementGroup(models.Model):
+    _inherit = 'procurement.group'
+
+    @api.model
+    def run(self, procurements, raise_user_error=True):
+        result = super(ProcurementGroup, self).run(procurements, raise_user_error)
+        def raise_exception(procurement_errors):
+            if raise_user_error:
+                dummy, errors = zip(*procurement_errors)
+                raise UserError('\n'.join(errors))
+            else:
+                raise ProcurementException(procurement_errors)
+        actions_to_run = defaultdict(list)
+        procurement_errors = []
+        for procurement in procurements:
+            procurement.values.setdefault('company_id', procurement.location_id.company_id)
+            procurement.values.setdefault('priority', '0')
+            procurement.values.setdefault('date_planned', fields.Datetime.now())
+            if float_is_zero(procurement.product_qty, precision_rounding=procurement.product_uom.rounding):
+                rule = self._get_rule(procurement.product_id, procurement.location_id, procurement.values)
+                if not rule:
+                    error = _('No rule has been found to replenish "%s" in "%s".\nVerify the routes configuration on the product.') %\
+                        (procurement.product_id.display_name, procurement.location_id.display_name)
+                    procurement_errors.append((procurement, error))
+                else:
+                    action = 'pull' if rule.action == 'pull_push' else rule.action
+                    actions_to_run[action].append((procurement, rule))
+                if procurement_errors:
+                    raise_exception(procurement_errors)
+                for action, procurements in actions_to_run.items():
+                    if hasattr(self.env['stock.rule'], '_run_%s' % action):
+                        try:
+                            getattr(self.env['stock.rule'], '_run_%s' % action)(procurements)
+                        except ProcurementException as e:
+                            procurement_errors += e.procurement_exceptions
+                    else:
+                        _logger.error("The method _run_%s doesn't exist on the procurement rules" % action)
+                if procurement_errors:
+                    raise_exception(procurement_errors)
+        return result
---
+++ sale_picking_generate_zero_lines/models/sale_order_line.py
@@ -0,0 +1,41 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+from odoo.tools import float_compare
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _action_launch_stock_rule(self, previous_product_uom_qty=False):
+        super(SaleOrderLine, self)._action_launch_stock_rule(previous_product_uom_qty)
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        procurements = []
+        for line in self:
+            line = line.with_company(line.company_id)
+            qty = line._get_qty_procurement(previous_product_uom_qty)
+            if float_compare(qty, line.product_uom_qty, precision_digits=precision) >= 0 and line.product_uom_qty == 0:
+                group_id = line._get_procurement_group()
+                if not group_id:
+                    group_id = self.env['procurement.group'].create(line._prepare_procurement_group_vals())
+                    line.order_id.procurement_group_id = group_id
+                else:
+                    updated_vals = {}
+                    if group_id.partner_id != line.order_id.partner_shipping_id:
+                        updated_vals.update({'partner_id': line.order_id.partner_shipping_id.id})
+                    if group_id.move_type != line.order_id.picking_policy:
+                        updated_vals.update({'move_type': line.order_id.picking_policy})
+                    if updated_vals:
+                        group_id.write(updated_vals)
+                values = line._prepare_procurement_values(group_id=group_id)
+                product_qty = line.product_uom_qty - qty
+                line_uom = line.product_uom
+                quant_uom = line.product_id.uom_id
+                product_qty, procurement_uom = line_uom._adjust_uom_quantities(product_qty, quant_uom)
+                procurements.append(self.env['procurement.group'].Procurement(
+                    line.product_id, product_qty, procurement_uom,
+                    line.order_id.partner_shipping_id.property_stock_customer,
+                    line.product_id.display_name, line.order_id.name, line.order_id.company_id, values))
+                if procurements:
+                    self.env['procurement.group'].run(procurements)
+        return True
