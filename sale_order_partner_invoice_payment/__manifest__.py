ac45cd6f258c790463146573a43d1fc21560b5ae
oihane <oihanecruce@gmail.com>
2024-01-30 15:13:46 +0100

    [FIX] sale_order_partner_invoice_payment: does not get the payment mode from invoice address (#356)

--- sale_order_partner_invoice_payment/__manifest__.py
+++ sale_order_partner_invoice_payment/__manifest__.py
@@ -2,16 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Partner Invoice Payment",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.1.0",
     "category": "Banking addons",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/sale-addons",
     "depends": [
         "sale",
         "account_payment_sale",
     ],
-    "data": [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
