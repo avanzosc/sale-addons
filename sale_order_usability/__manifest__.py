394615c8ea1b4476f48a81ad72b4612159fc24b2
Tu Nombre <you@example.com>
2024-02-05 14:41:24 +0100

    [14.0][IMP] sale_order_usability: Delivered qty in sale order line kanban view.

--- sale_order_usability/__manifest__.py
+++ sale_order_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Usability",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Sales/Sales",
     "license": "AGPL-3",
     "author": "AvanzOSC",
