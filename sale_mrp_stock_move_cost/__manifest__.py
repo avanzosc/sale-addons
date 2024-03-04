d86c4a04c3b581cfc4df6c9f96838ee9511f645e
Alfredo <alfredodelafuente@avanzosc.es>
2024-03-04 10:35:55 +0100

    [ADD] sale_mrp_stock_move_cost: In sale order new fields "Mo's Cost", and "Sale margin with Mo's".

---
+++ sale_mrp_stock_move_cost/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Mrp Stock Move Cost",
+    "version": "16.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "sale",
+        "mrp_stock_move_cost"
+    ],
+    "excludes": [],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
