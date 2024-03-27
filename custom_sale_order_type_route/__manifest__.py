7ada07effe9f2f617b354d745656077c259602ea
Tu Nombre <you@example.com>
2024-03-27 10:55:05 +0100

    [14.0][ADD] custom_sale_order_type_route: New deliveremen menu.

---
+++ custom_sale_order_type_route/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Custom Sale Order Type Route",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale_stock",
+        "sale_order_type",
+        "sale_order_user_usability",
+        "sale_line_pending_info",
+        "sale_order_line_qty_by_packaging",
+        "custom_saca_intercompany",
+        "sale_order_line_containers",
+    ],
+    "data": [
+        "views/sale_order_type_view.xml",
+        "views/sale_order_view.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
