263101046e1380cae4d0a24a2971a0bc72dbc0c2
Alfredo <alfredodelafuente@avanzosc.es>
2024-04-24 09:21:39 +0200

    [ADD] sale_order_type_term: "Terms and Conditions" description in sale order type.

---
+++ sale_order_type_term/models/sale_order.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange("type_id")
+    def onchange_type_id(self):
+        result = super(SaleOrder, self).onchange_type_id()
+        for order in self.filtered(
+                lambda x: x.type_id and x.type_id.description):
+            order.note = order.type_id.description
+        return result
