e3784163706265916d303e86ac2b744c21af6391
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-04-30 13:16:31 +0200

    [ADD] sale_order_type_term: "Terms and Conditions" description in sale order type. (#386)

---
+++ sale_order_type_term/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Type Term",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "category": "Warehouse",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_order_type",
+    ],
+    "data": [
+        "views/sale_order_type_views.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_order_type_term/models/sale_order_type.py
@@ -0,0 +1,11 @@
+# Copyright 2024 Alfredo de la fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import fields, models
+
+
+class SaleOrderType(models.Model):
+    _inherit = "sale.order.type"
+
+    description = fields.Text(
+        string="Terms and Conditions"
+    )
