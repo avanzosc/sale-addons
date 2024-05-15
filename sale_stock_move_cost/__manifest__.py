309b9a0397b5391dd8af543a62509feb74193834
Alfredo <alfredodelafuente@avanzosc.es>
2024-05-15 13:36:36 +0200

    [IMP] sale_stock_move_cost: Mostrar coste y margen en tree de líneas de pedido de venta.

--- sale_stock_move_cost/__manifest__.py
+++ sale_stock_move_cost/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Stock Move Cost",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Sales",
     "license": "AGPL-3",
     "author": "AvanzOSC",
@@ -14,6 +14,7 @@
     "excludes": [],
     "data": [
         "views/sale_order_views.xml",
+        "views/sale_order_line_views.xml",
     ],
     "installable": True,
     "auto_install": True,
