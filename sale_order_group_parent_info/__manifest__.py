e38f814a9a21b141533f6488a47da6b33cf113a8
Alfredo <alfredodelafuente@avanzosc.es>
2024-05-15 08:13:20 +0200

    [ADD] sale_order_group_parent_info: Group sale orders by "Main Partner" and "Main Province".

---
+++ sale_order_group_parent_info/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Group Parent Info",
+    "version": "14.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://www.avanzosc.es/",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml"
+    ],
+    "installable": True,
+}
