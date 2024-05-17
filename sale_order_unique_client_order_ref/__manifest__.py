66d999493c394b74e99e19d7ce7a1d592b970454
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-05-17 11:53:10 +0200

    [ADD] sale_order_unique_client_order_ref: In sale order new field "Unique ref/description control?". (#392)

---
+++ sale_order_unique_client_order_ref/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+#   Copyright 2023 Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Unique Client Order",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzos.ces",
+    "category": "Sales Management",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
