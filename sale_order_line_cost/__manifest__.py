826b7fa8b865426ccec62e7c7bfcb9b089aed1d4
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:39:05 +0200

    [IMP] sale_order_line_cost: pre-commit stuff

--- sale_order_line_cost/__manifest__.py
+++ sale_order_line_cost/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Line Cost",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Sale",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/sale-addons",
     "depends": [
         "product",
         "sale_order_line_menu",
@@ -13,8 +13,8 @@
     "data": [
         "views/product_template_views.xml",
         "views/sale_order_views.xml",
-        "views/sale_order_line_views.xml"
+        "views/sale_order_line_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
