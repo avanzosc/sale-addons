58b25f44e419e561c5bcfa0c13d6227de6db7015
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:30:09 +0200

    [IMP] sale_order_to_payment: pre-commit stuff

--- sale_order_to_payment/__manifest__.py
+++ sale_order_to_payment/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order To Payment",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "category": "Analytic",
     "license": "AGPL-3",
     "author": "AvanzOSC",
@@ -15,5 +15,5 @@
     "data": [
         "views/sale_order_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- sale_order_to_payment/models/sale_order.py
+++ sale_order_to_payment/models/sale_order.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import _, api, fields, models
+from odoo import _, fields, models
 
 
 class SaleOrder(models.Model):
@@ -9,10 +9,11 @@ class SaleOrder(models.Model):
     payment_ids = fields.Many2many(
         string="Payments",
         comodel_name="account.payment",
-        compute="_compute_payment_ids")
+        compute="_compute_payment_ids",
+    )
     payment_count = fields.Integer(
-        string="Payments Count",
-        compute="_compute_payment_count")
+        string="Payments Count", compute="_compute_payment_count"
+    )
 
     def _compute_payment_count(self):
         for sale in self:
@@ -32,13 +33,12 @@ class SaleOrder(models.Model):
     def action_view_payments(self):
         context = self.env.context.copy()
         if self.partner_id:
-            context.update({
-                "default_partner_id": self.partner_id.id})
+            context.update({"default_partner_id": self.partner_id.id})
         return {
             "name": _("Payments"),
             "view_mode": "tree,form",
             "res_model": "account.payment",
             "domain": [("id", "in", self.payment_ids.ids)],
             "type": "ir.actions.act_window",
-            "context": context
-            }
+            "context": context,
+        }
