566176ff19d79504d3c3629e1921db8374a6886b
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 10:11:33 +0200

    [IMP] sale_stock_move_cost: pre-commit stuff

--- sale_stock_move_cost/models/sale_order_line.py
+++ sale_stock_move_cost/models/sale_order_line.py
@@ -7,15 +7,21 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     sale_line_cost = fields.Float(
-        string="Line Cost", digits="Product Price", copy=False, store=True,
-        compute="_compute_sale_line_cost_margin"
+        string="Line Cost",
+        digits="Product Price",
+        copy=False,
+        store=True,
+        compute="_compute_sale_line_cost_margin",
     )
     sale_line_margin = fields.Float(
-        string="Line margin ", digits="Product Price", copy=False,
-        store=True, compute="_compute_sale_line_cost_margin")
+        string="Line Margin",
+        digits="Product Price",
+        copy=False,
+        store=True,
+        compute="_compute_sale_line_cost_margin",
+    )
 
-    @api.depends("move_ids", "move_ids.move_line_ids",
-                 "move_ids.move_line_ids.cost")
+    @api.depends("move_ids", "move_ids.move_line_ids", "move_ids.move_line_ids.cost")
     def _compute_sale_line_cost_margin(self):
         for line in self:
             sale_line_cost = 0
