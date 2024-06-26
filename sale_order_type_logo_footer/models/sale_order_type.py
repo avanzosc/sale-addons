6f3aa0b1033ce5a1f2c632eef872f2da50698405
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 16:01:00 +0200

    [IMP] sale_order_type_logo_footer: pre-commit stuff

--- sale_order_type_logo_footer/models/sale_order_type.py
+++ sale_order_type_logo_footer/models/sale_order_type.py
@@ -7,7 +7,5 @@ from odoo import fields, models
 class SaleOrderType(models.Model):
     _inherit = "sale.order.type"
 
-    logo_for_reports = fields.Binary(
-        "Logo for reports")
-    footer_for_reports = fields.Text(
-        "Footer for reports", translate=True)
+    logo_for_reports = fields.Binary("Logo for reports")
+    footer_for_reports = fields.Text("Footer for reports", translate=True)
