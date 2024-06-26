cf531080cf952eca9c4af926865ab8fb0b0d1f44
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:19:06 +0200

    [IMP] sale_address_domain: pre-commit stuff

--- sale_address_domain/__manifest__.py
+++ sale_address_domain/__manifest__.py
@@ -2,16 +2,14 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Sale Address Domain",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "sale",
     ],
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/sale-addons",
     "category": "Sales",
-    "data": [
-        "views/sale_order_view.xml",
-    ],
+    "data": [],
     "installable": True,
 }
