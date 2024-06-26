3b3e1841079f40234da1709cbe4fab09113e922e
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:41:40 +0200

    [IMP] sale_order_group_parent_info: pre-commit stuff

--- sale_order_group_parent_info/models/sale_order.py
+++ sale_order_group_parent_info/models/sale_order.py
@@ -7,18 +7,25 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     main_parent_id = fields.Many2one(
-        string="Main Partner", comodel_name="res.partner",
-        related="partner_id.commercial_partner_id", store=True, copy=False
+        string="Main Partner",
+        comodel_name="res.partner",
+        related="partner_id.commercial_partner_id",
+        store=True,
+        copy=False,
     )
     main_state_id = fields.Many2one(
-        string="Main Province", comodel_name="res.country.state",
-        compute="_compute_main_state_id", store=True, copy=False
+        string="Main Province",
+        comodel_name="res.country.state",
+        compute="_compute_main_state_id",
+        store=True,
+        copy=False,
     )
 
     @api.depends("main_parent_id", "main_parent_id.state_id")
     def _compute_main_state_id(self):
         for sale in self:
             sale.main_state_id = (
-                sale.main_parent_id.state_id.id if sale.main_parent_id.state_id
+                sale.main_parent_id.state_id.id
+                if sale.main_parent_id.state_id
                 else False
             )
