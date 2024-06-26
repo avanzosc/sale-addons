fa3750d5f2a6395b2e85daff0287c1a3e8238c80
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:44:14 +0200

    [IMP] sale_order_confirm_usability: pre-commit stuff

--- sale_order_confirm_usability/__manifest__.py
+++ sale_order_confirm_usability/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Confirm Usability",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "category": "Analytic",
     "license": "AGPL-3",
     "author": "AvanzOSC",
@@ -18,5 +18,5 @@
         "views/sale_order_view.xml",
         "views/stock_picking_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
