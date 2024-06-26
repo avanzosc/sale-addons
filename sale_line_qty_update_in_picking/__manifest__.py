2b37b951237355ac77af702ac92062671efe63bd
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:44:01 +0200

    [IMP] sale_line_qty_update_in_picking: pre-commit stuff

--- sale_line_qty_update_in_picking/__manifest__.py
+++ sale_line_qty_update_in_picking/__manifest__.py
@@ -2,15 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Line Qty Update In Picking",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Inventory",
     "website": "https://github.com/avanzosc/sale-addons",
     "depends": [
         "sale_stock",
     ],
-    "data": [
-    ],
+    "data": [],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
