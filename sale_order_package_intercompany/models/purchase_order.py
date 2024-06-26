df2108cbb2e8f897e6d91e5d13d28b9d610ede83
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 16:07:27 +0200

    [IMP] sale_order_package_intercompany: pre-commit stuff

--- sale_order_package_intercompany/models/purchase_order.py
+++ sale_order_package_intercompany/models/purchase_order.py
@@ -8,9 +8,13 @@ class PurchaseOrder(models.Model):
 
     def _prepare_sale_order_line_data(self, purchase_line, dest_company, sale_order):
         result = super(PurchaseOrder, self)._prepare_sale_order_line_data(
-            purchase_line, dest_company, sale_order)
+            purchase_line, dest_company, sale_order
+        )
         if purchase_line.product_packaging:
-            result.update({
-                "product_packaging": purchase_line.product_packaging.id,
-                "product_packaging_qty": purchase_line.product_packaging_qty})
+            result.update(
+                {
+                    "product_packaging": purchase_line.product_packaging.id,
+                    "product_packaging_qty": purchase_line.product_packaging_qty,
+                }
+            )
         return result
