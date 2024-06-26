afb1a97e170b25af678cde5c8b7936c67d39c8c3
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:40:09 +0200

    [IMP] sale_order_line_containers: pre-commit stuff

--- sale_order_line_containers/models/sale_order_line.py
+++ sale_order_line_containers/models/sale_order_line.py
@@ -1,11 +1,10 @@
 # © 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    container = fields.Integer(
-        string="Container")
+    container = fields.Integer(string="Container")
