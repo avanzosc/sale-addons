eeaccd5f4221f30c399d1aa9baefe623912f2022
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 16:07:27 +0200

    [IMP] sale_order_package_intercompany: pre-commit stuff

--- sale_order_package_intercompany/__manifest__.py
+++ sale_order_package_intercompany/__manifest__.py
@@ -2,17 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Package Inter Company",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/sale-addons",
     "category": "Sale",
     "depends": [
         "sale_order_line_qty_by_packaging",
         "purchase_order_line_packaging_qty",
         "custom_saca_intercompany",
     ],
-    "data": [
-    ],
+    "data": [],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
--- sale_order_package_intercompany/models/stock_picking.py
+++ sale_order_package_intercompany/models/stock_picking.py
@@ -9,19 +9,39 @@ class StockPicking(models.Model):
     def button_validate(self):
         result = super(StockPicking, self).button_validate()
         for picking in self:
-            if picking.sale_id and (
-                picking.sale_id.auto_purchase_order_id) and (
-                    picking.state == "done"):
+            if (
+                picking.sale_id
+                and (picking.sale_id.auto_purchase_order_id)
+                and (picking.state == "done")
+            ):
                 for moveline in picking.move_line_ids_without_package:
                     if moveline.product_packaging_id:
-                        purchase_picking = picking.sale_id.sudo(
-                            ).auto_purchase_order_id.picking_ids.filtered(
-                        lambda c: c.state not in ('cancel', 'done'))[:1]
+                        purchase_pickings = (
+                            picking.sale_id.sudo().auto_purchase_order_id.mapped(
+                                "picking_ids"
+                            )
+                        )
+                        purchase_picking = purchase_pickings.filtered(
+                            lambda c: c.state not in ("cancel", "done")
+                        )[:1]
                         if purchase_picking:
-                            purchase_line = purchase_picking.move_line_ids_without_package.filtered(lambda c: c.product_id == moveline.product_id and c.lot_id.name == moveline.lot_id.name)
+                            purchase_mlines = (
+                                purchase_picking.move_line_ids_without_package
+                            )
+                            purchase_line = purchase_mlines.filtered(
+                                lambda c: c.product_id == moveline.product_id
+                                and c.lot_id.name == moveline.lot_id.name
+                            )
                             if not purchase_line:
-                                purchase_line = purchase_picking.move_line_ids_without_package.filtered(lambda c: c.product_id == moveline.product_id and not c.lot_id)
+                                purchase_line = purchase_mlines.filtered(
+                                    lambda c: c.product_id == moveline.product_id
+                                    and not c.lot_id
+                                )
                             if purchase_line:
-                                purchase_line.product_packaging_id = moveline.product_packaging_id.id
-                                purchase_line.product_packaging_qty = moveline.product_packaging_qty
+                                purchase_line.product_packaging_id = (
+                                    moveline.product_packaging_id.id
+                                )
+                                purchase_line.product_packaging_qty = (
+                                    moveline.product_packaging_qty
+                                )
         return result
