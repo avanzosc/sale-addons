09b076d4042140e73704733f0c802e76be25d6bc
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 10:25:22 +0200

    [IMP] sale_order_unique_client_order_ref: pre-commit stuff

--- sale_order_unique_client_order_ref/__manifest__.py
+++ sale_order_unique_client_order_ref/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- encoding: utf-8 -*-
 #   Copyright 2023 Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Order Unique Client Order",
     "author": "AvanzOSC",
-    "website": "http://www.avanzos.ces",
+    "website": "https://github.com/avanzosc/sale-addons",
     "category": "Sales Management",
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
--- sale_order_unique_client_order_ref/models/sale_order.py
+++ sale_order_unique_client_order_ref/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 #   Copyright 2023 Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
@@ -9,18 +8,25 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     control_unique_client_order_ref = fields.Boolean(
-        string="Unique ref/description control?",
-        default=True, copy=False)
+        string="Unique ref/description control?", default=True, copy=False
+    )
 
     @api.model
     def create(self, vals):
         if "client_order_ref" in vals and vals.get("client_order_ref", False):
             cond = [("client_order_ref", "=", vals.get("client_order_ref"))]
             sale = self.env["sale.order"].search(cond, limit=1)
-            if (sale and "control_unique_client_order_ref" in vals and
-                    vals.get("control_unique_client_order_ref", False)):
-                error = _(u"The sales order {} already exists, with customer "
-                          "reference: {}").format(
-                              sale.name, sale.client_order_ref)
+            if (
+                sale
+                and "control_unique_client_order_ref" in vals
+                and vals.get("control_unique_client_order_ref", False)
+            ):
+                error = _(
+                    "The sales order %(order_ref)s already exists, with customer"
+                    " reference: %(customer_ref)s"
+                ) % {
+                    "order_ref": sale.name,
+                    "customer_ref": sale.client_order_ref,
+                }
                 raise ValidationError(error)
-        return super(SaleOrder, self).create(vals)
+        return super().create(vals)
