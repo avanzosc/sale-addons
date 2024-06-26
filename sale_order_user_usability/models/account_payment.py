20739e39d47ee2e37048f8d281ed6cfeacbc582f
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 15:23:08 +0200

    [IMP] sale_order_user_usability: pre-commit stuff

--- sale_order_user_usability/models/account_payment.py
+++ sale_order_user_usability/models/account_payment.py
@@ -7,6 +7,4 @@ from odoo import fields, models
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
-    user_id = fields.Many2one(
-        string="Commercial",
-        comodel_name="res.users")
+    user_id = fields.Many2one(string="Commercial", comodel_name="res.users")
