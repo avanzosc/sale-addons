bf3827ae0e37f0c609fc977a967270c8691e7557
Unai Beristain <unaiberis@gmail.com>
2024-07-04 15:08:02 +0200

    [IMP] Pre-commit

--- sale_project_planned_hours_compute/models/project_project.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ProjectProject(models.Model):
-    _inherit = "project.project"
-
-    sale_hourly_rate = fields.Float(string="Sale Hourly Rate", default=50)
