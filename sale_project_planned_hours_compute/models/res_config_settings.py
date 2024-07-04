2f7dd743460c5ade32568fe2a069368fc571bd16
Unai Beristain <unaiberis@gmail.com>
2024-07-04 15:08:35 +0200

    [IMP] Pre-commit

--- sale_project_planned_hours_compute/models/res_config_settings.py
+++ sale_project_planned_hours_compute/models/res_config_settings.py
@@ -4,4 +4,8 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    sale_hourly_rate = fields.Float(string="Sale Hourly Rate", default=50)
+    sale_hourly_rate = fields.Float(
+        string="Sale Hourly Rate",
+        default=50,
+        config_parameter="sale_project_planned_hours_compute.sale_hourly_rate",
+    )
--- sale_project_planned_hours_compute/models/sale_order_line.py
+++ sale_project_planned_hours_compute/models/sale_order_line.py
@@ -1,7 +1,5 @@
-import logging
 from odoo import api, fields, models
 
-_logger = logging.getLogger(__name__)
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
@@ -14,24 +12,23 @@ class SaleOrderLine(models.Model):
 
     @api.depends("task_id.planned_hours")
     def _compute_order_project_sale_hourly_rate(self):
-        _logger.info("Starting _compute_order_project_sale_hourly_rate")
         temp_sale_hourly_rate = float(
-            self.env["res.config.settings"].sudo().search([], limit=1).sale_hourly_rate
+            float(
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param(
+                    "sale_project_planned_hours_compute.sale_hourly_rate", default=50
+                )
+            )
         )
-        _logger.info(f"Retrieved sale hourly rate: {temp_sale_hourly_rate}")
 
         for line in self:
             line.order_project_sale_hourly_rate = temp_sale_hourly_rate
-            _logger.info(f"Set order_project_sale_hourly_rate: {temp_sale_hourly_rate} for line {line.id}")
             if line.order_id.state == "sale" and line.task_id:
-                _logger.info(f"Order is in 'sale' state and task_id is present for line {line.id}")
                 if line.product_uom_qty > 0 and temp_sale_hourly_rate > 0:
                     hours = line.price_subtotal / temp_sale_hourly_rate
-                    _logger.info(f"Computed planned hours: {hours} for line {line.id}")
-
                     line.task_id.planned_hours = hours
                 else:
-                    _logger.info(f"Set planned hours to 0 for line {line.id}")
                     line.task_id.planned_hours = 0
 
     @api.onchange(
@@ -39,21 +36,20 @@ class SaleOrderLine(models.Model):
         "price_subtotal",
     )
     def _compute_planned_hours(self):
-        _logger.info("Starting _compute_planned_hours")
         temp_sale_hourly_rate = float(
-            self.env["res.config.settings"].sudo().search([], limit=1).sale_hourly_rate
+            float(
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param(
+                    "sale_project_planned_hours_compute.sale_hourly_rate", default=50
+                )
+            )
         )
-        _logger.info(f"Retrieved sale hourly rate: {temp_sale_hourly_rate}")
 
         for line in self:
-            _logger.info(f"Processing line {line.id}")
             if line.order_id.state == "sale" and line.task_id:
-                _logger.info(f"Order is in 'sale' state and task_id is present for line {line.id}")
                 if line.product_uom_qty > 0 and temp_sale_hourly_rate > 0:
                     hours = line.price_subtotal / temp_sale_hourly_rate
-                    _logger.info(f"Computed planned hours: {hours} for line {line.id}")
-
                     line.task_id.planned_hours = hours
                 else:
-                    _logger.info(f"Set planned hours to 0 for line {line.id}")
                     line.task_id.planned_hours = 0
