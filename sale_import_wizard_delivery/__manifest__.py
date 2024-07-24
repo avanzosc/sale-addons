67019f14d681022a3d3c5ef95d810e0ae6791899
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 12:02:15 +0200

    [14.0][ADD] sale_import_wizard_delivery: In "Sale import wizard" new field "Shipping Method".

---
+++ sale_import_wizard_delivery/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Import Wizard Delivery",
+    "version": "14.0.1.0.0",
+    "category": "Hidden/Tools",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": ["sale_import_wizard", "delivery"],
+    "data": ["views/sale_order_import_views.xml"],
+    "installable": True,
+    "auto_install": True,
+}
