68b01da2be3321a1eb40a35f515f9544e81f578f
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 08:44:23 +0200

    [16.0][ADD] sale_order_private_note: Private note in sale order.

---
+++ sale_order_private_note/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Private Note",
+    "version": "16.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "excludes": [],
+    "data": [
+        "data/sale_order_private_note_group.xml",
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
