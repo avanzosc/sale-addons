c992bdcadef128541408ddbaa807b11bc62f18f5
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 09:14:29 +0200

    [14.0][ADD] sale_order_line_input_planned_quantity: Show in the editable tree of sales order lines, the fields: "Planned Quantity", and "Difference Between Ordered And Planned".

---
+++ sale_order_line_input_planned_quantity/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2024 Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Line Input Planned Quantity",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "sale_order_line_input",
+        "sale_order_line_planned_quantity",
+    ],
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "data": [
+        "views/sale_order_line_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
