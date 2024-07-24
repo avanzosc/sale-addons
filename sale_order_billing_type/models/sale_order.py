e612afe7665796436ccdb0822a83a1bd9c12e346
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 08:32:47 +0200

    [16.0][ADD] sale_order_billing_type: In Sales-Configuration, new menu for new object "Billing types".

---
+++ sale_order_billing_type/models/sale_order.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    billing_type_id = fields.Many2one(
+        string="Billing type",
+        comodel_name="res.partner.billing.type",
+        related="partner_id.billing_type_id",
+        store=True,
+        copy=False,
+    )
