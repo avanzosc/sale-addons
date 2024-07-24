1ac5330f336886fb164b444071ae69706b651158
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 08:44:23 +0200

    [16.0][ADD] sale_order_private_note: Private note in sale order.

---
+++ sale_order_private_note/models/sale_order.py
@@ -0,0 +1,9 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    private_note = fields.Text(copy=False)
