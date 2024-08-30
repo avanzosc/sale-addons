f4168e8531c167c40e7103fa7e3984ac1bd21caf
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-08-30 09:45:44 +0200

    [IMP] sale_order_line_planned_quantity: computation similar to delivered

--- sale_order_line_planned_quantity/models/sale_order_line.py
+++ sale_order_line_planned_quantity/models/sale_order_line.py
@@ -17,50 +17,45 @@ class SaleOrderLine(models.Model):
         string="Difference Between Ordered And Planned",
         store=True,
         copy=False,
-        compute="_compute_difference_between_ordered_planned",
+        compute="_compute_planned_quantity",
     )
 
     @api.depends(
         "product_uom_qty",
-        "state",
-        "move_ids",
         "move_ids.state",
+        "move_ids.scrapped",
         "move_ids.product_uom_qty",
-        "move_ids.picking_type_id",
-        "move_ids.picking_type_id.use_to_calculate_planned_quantities",
+        "move_ids.product_uom",
     )
     def _compute_planned_quantity(self):
         for line in self:
-            planned_quantity = 0
-            moves_planned_quantities = False
-            if line.move_ids:
-                moves_planned_quantities = line.move_ids.filtered(
-                    lambda x: x.picking_type_id.use_to_calculate_planned_quantities
-                )
-                if moves_planned_quantities:
-                    moves = moves_planned_quantities.filtered(
-                        lambda x: x.state != "cancel"
+            # TODO: maybe one day, this should be done in SQL for performance sake
+            if line.qty_delivered_method == "stock_move":
+                planned_qty = 0.0
+                outgoing_moves, incoming_moves = line._get_outgoing_incoming_moves()
+                for move in outgoing_moves:
+                    if move.state == "cancel":
+                        continue
+                    planned_qty += move.product_uom._compute_quantity(
+                        move.product_uom_qty,
+                        line.product_uom,
+                        rounding_method="HALF-UP",
                     )
-                    if moves:
-                        planned_quantity = sum(moves.mapped("product_uom_qty"))
-            line.planned_quantity = planned_quantity if moves_planned_quantities else 0
-
-    @api.depends(
-        "planned_quantity",
-        "state",
-        "product_uom_qty",
-    )
-    def _compute_difference_between_ordered_planned(self):
-        for line in self:
-            moves_planned_quantities = False
-            if line.move_ids:
-                moves_planned_quantities = line.move_ids.filtered(
-                    lambda x: x.picking_type_id.use_to_calculate_planned_quantities
-                )
-            if moves_planned_quantities:
-                line.difference_between_ordered_planned = bool(
-                    line.product_uom_qty != line.planned_quantity
-                    and line.state != "draft"
-                )
+                for move in incoming_moves:
+                    if move.state == "cancel":
+                        continue
+                    planned_qty -= move.product_uom._compute_quantity(
+                        move.product_uom_qty,
+                        line.product_uom,
+                        rounding_method="HALF-UP",
+                    )
             else:
-                line.difference_between_ordered_planned = False
+                planned_qty = line.product_uom_qty
+            line.update(
+                {
+                    "planned_quantity": planned_qty,
+                    "difference_between_ordered_planned": bool(
+                        line.product_uom_qty != planned_qty
+                    ),
+                }
+            )
