cbad1e98172656e53e9eeb1f5a841131b17b7da8
Alfredo <alfredodelafuente@avanzosc.es>
2024-09-13 09:09:51 +0200

    [16.0][ADD] sale_order_client_order_ref_in_head: Display the "Customer reference" field in the sale order tree view.

---
+++ sale_order_client_order_in_header/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Client Order In Header",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
