ca3fad2ffa68425652e088d23162291f6a6c5b59
Unai Beristain <unaiberis@gmail.com>
2024-09-17 16:06:03 +0200

    [ADD] sale_order_line_commitment_date_and_delivery_address

---
+++ sale_order_line_commitment_date_and_delivery_address/models/sale_order_line.py
@@ -0,0 +1,15 @@
+from odoo import models, fields
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    commitment_date = fields.Date(
+        related="order_id.commitment_date",
+        string="Commitment Date",
+        store=True,
+    )
+    delivery_address = fields.Char(
+        related="order_id.partner_id.contact_address",
+        string="Delivery Address",
+        store=True,
+    )
