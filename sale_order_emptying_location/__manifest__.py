8269d31453df642b901a47fc3af7dfb470d74334
Tu Nombre <you@example.com>
2024-09-17 13:56:10 +0200

    [14.0][ADD] sale_order_emptying_location: Button in sale orders to emtying the sale type location.

---
+++ sale_order_emptying_location/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Emptying Location",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale_order_type_picking",
+        "sale_order_lot_selection",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
