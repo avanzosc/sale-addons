d3635162db8f8cad938a2e1129c52ab823824eee
Alfredo <alfredodelafuente@avanzosc.es>
2024-09-18 10:32:13 +0200

    [16.0][ADD] sale_blanket_order_description: In "Blanket Order" new field: "Description".

---
+++ sale_blanket_order_description/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale blanket Order Description",
+    "version": "16.0.1.0.0",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale_blanket_order",
+    ],
+    "data": [
+        "report/sale_blanket_order_report.xml",
+        "views/sale_blanket_order_views.xml",
+    ],
+    "installable": True,
+}
