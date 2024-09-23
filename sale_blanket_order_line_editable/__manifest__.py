75ffd4ce1bc0f122bfe3d9a7adb3f27ff49164b6
Alfredo <alfredodelafuente@avanzosc.es>
2024-09-23 09:46:48 +0200

    [16.0][ADD] sale_blanket_order_line_editable: Blanket order lines read only where state in "Done" or "Expired".

---
+++ sale_blanket_order_line_editable/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale blanket Order Line Editable",
+    "version": "16.0.1.0.0",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale_blanket_order",
+    ],
+    "data": [
+        "views/sale_blanket_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
