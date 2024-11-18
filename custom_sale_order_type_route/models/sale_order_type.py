dc6c7e9c0de52cf9a87993aa0b09aa8ef5999c1e
Berezi <berezi@avanzosc.home>
2024-11-18 14:45:28 +0100

    [14.0][IMP] custom_sale_order_type_route: Change create picking function.

--- custom_sale_order_type_route/models/sale_order_type.py
+++ custom_sale_order_type_route/models/sale_order_type.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class SaleOrderType(models.Model):
@@ -14,3 +14,22 @@ class SaleOrderType(models.Model):
     burden_picking_type = fields.Many2one(
         string="Burden Picking Type", comodel_name="stock.picking.type"
     )
+
+    def action_view_contact(self):
+        context = self.env.context.copy()
+        context.update(
+            {
+                "default_sale_type": self.id,
+            }
+        )
+        return {
+            "name": _("Contacts"),
+            "view_mode": "tree",
+            "view_id": self.env.ref(
+                "custom_sale_order_type_route.view_partner_tree_editable"
+            ).id,
+            "res_model": "res.partner",
+            "domain": [("sale_type", "=", self.id)],
+            "type": "ir.actions.act_window",
+            "context": context,
+        }
