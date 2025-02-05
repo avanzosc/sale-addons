e67f9aa5a560de6a8fc6797c77ede10b15696804
Alfredo <alfredodelafuente@avanzosc.es>
2025-02-05 13:21:48 +0100

    [16.0][ADD] sale_order_head_origin: In sale order form, show "origin" field in head.

---
+++ sale_order_head_origin/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Head Origin",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Sales/Sales",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
