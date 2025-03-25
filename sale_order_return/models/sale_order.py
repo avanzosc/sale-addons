90560b08b869dbe9c59c066f48209800ed97016e
Berezi <berezi@avanzosc.home>
2025-03-25 08:15:58 +0100

    [14.0][IMP] sale_order_return: Optimize return button.

--- sale_order_return/models/sale_order.py
+++ sale_order_return/models/sale_order.py
@@ -26,24 +26,22 @@ class SaleOrder(models.Model):
         if not self.picking_ids and any(
             [line.product_uom_qty != 0 for line in self.order_line]
         ):
-            raise ValidationError(_("First you have to do the delivery."))
-        elif not self.picking_ids and any(
-            [line.return_qty != 0 for line in self.order_line]
-        ):
-            if not self.type_id:
-                raise ValidationError(_("You must enter the order type."))
-            elif not self.type_id.picking_type_id:
-                raise ValidationError(
-                    _("You must enter the picking type of the order type.")
-                )
-            elif not self.type_id.picking_type_id.return_picking_type_id:
-                raise ValidationError(
-                    _(
-                        "You must enter the return picking type of picking type of the"
-                        " order type."
+            if any(line.product_uom_qty != 0 for line in self.order_line):
+                raise ValidationError(_("First you have to do the delivery."))
+            if any(line.return_qty != 0 for line in self.order_line):
+                if not self.type_id:
+                    raise ValidationError(_("You must enter the order type."))
+                if not self.type_id.picking_type_id:
+                    raise ValidationError(
+                        _("You must enter the picking type of the order type.")
                     )
-                )
-            else:
+                if not self.type_id.picking_type_id.return_picking_type_id:
+                    raise ValidationError(
+                        _(
+                            "You must enter the return picking type of the "
+                            + "order type's picking type."
+                        )
+                    )
                 self.action_confirm()
                 entry_picking = self.picking_ids[:1]
                 entry_picking.button_force_done_detailed_operations()
@@ -59,62 +57,63 @@ class SaleOrder(models.Model):
                     }
                 )
                 picking.group_id.sale_id = self.id
-                # for line in self.order_line:
-                #     previous_product_uom_qty = {
-                #         ln.id: ln.product_uom_qty for ln in line
-                #     }
-                #     line._action_launch_stock_rule(previous_product_uom_qty)
+                location_id = picking.location_id.id
+                location_dest_id = picking.location_dest_id.id
+                move_vals_list = []
                 for line in entry_picking.move_line_ids_without_package:
-                    move = self.env["stock.move"].create(
-                        {
-                            "sale_line_id": line.move_id.sale_line_id.id,
-                            "picking_id": picking.id,
-                            "product_id": line.product_id.id,
-                            "name": line.move_id.name,
-                            "product_uom": line.product_uom_id.id,
-                            "product_uom_qty": line.move_id.sale_line_id.return_qty,
-                            "location_id": picking.location_id.id,
-                            "location_dest_id": picking.location_dest_id.id,
-                            "move_orig_ids": [(4, line.move_id.id)],
-                            "origin_returned_move_id": line.move_id.id,
-                            "to_refund": True,
-                            "move_line_ids": [
-                                (
-                                    0,
-                                    0,
-                                    {
-                                        "picking_id": picking.id,
-                                        "product_id": line.product_id.id,
-                                        "product_uom_id": line.product_uom_id.id,
-                                        "qty_done": line.move_id.sale_line_id.return_qty,
-                                        "location_id": picking.location_id.id,
-                                        "location_dest_id": picking.location_dest_id.id,
-                                        "lot_id": line.move_id.sale_line_id.lot_id.id,
-                                    },
-                                )
-                            ],
-                        }
-                    )
+                    sale_line = line.move_id.sale_line_id
+                    return_qty = sale_line.return_qty
+                    move_vals = {
+                        "sale_line_id": sale_line.id,
+                        "picking_id": picking.id,
+                        "product_id": line.product_id.id,
+                        "name": line.move_id.name,
+                        "product_uom": line.product_uom_id.id,
+                        "product_uom_qty": return_qty,
+                        "location_id": location_id,
+                        "location_dest_id": location_dest_id,
+                        "move_orig_ids": [(4, line.move_id.id)],
+                        "origin_returned_move_id": line.move_id.id,
+                        "to_refund": True,
+                        "move_line_ids": [
+                            (
+                                0,
+                                0,
+                                {
+                                    "picking_id": picking.id,
+                                    "product_id": line.product_id.id,
+                                    "product_uom_id": line.product_uom_id.id,
+                                    "qty_done": return_qty,
+                                    "location_id": location_id,
+                                    "location_dest_id": location_dest_id,
+                                    "lot_id": sale_line.lot_id.id,
+                                },
+                            )
+                        ],
+                    }
+                    move_vals_list.append(move_vals)
+                moves = self.env["stock.move"].create(move_vals_list)
+                for line, move in zip(
+                    entry_picking.move_line_ids_without_package, moves
+                ):
                     line.move_id.write(
                         {
                             "move_dest_ids": [(4, move.id)],
                             "returned_move_ids": [(4, move.id)],
                         }
                     )
-                picking.group_id = self.procurement_group_id.id
                 picking.action_confirm()
                 entry_picking.action_cancel()
                 entry_picking.sudo().unlink()
                 return picking.button_validate()
-        else:
-            picking_act = super(SaleOrder, self).button_return_picking()
-            picking = self.env["stock.picking"].browse(picking_act.get("res_id"))
-            if len(picking) == 1:
-                for line in picking.move_line_ids_without_package:
-                    if line.move_id.sale_line_id:
-                        line.qty_done = line.move_id.sale_line_id.return_qty
-                if not any([line.return_qty != 0 for line in self.order_line]):
-                    return picking_act
-                else:
-                    picking.action_confirm()
-                    return picking.button_validate()
+        picking_act = super(SaleOrder, self).button_return_picking()
+        picking = self.env["stock.picking"].browse(picking_act.get("res_id"))
+        if len(picking) == 1:
+            for line in picking.move_line_ids_without_package:
+                if line.move_id.sale_line_id:
+                    line.qty_done = line.move_id.sale_line_id.return_qty
+            return (
+                picking_act
+                if not any(line.return_qty != 0 for line in self.order_line)
+                else picking.button_validate()
+            )
