140a132490e968d06edd3ba2008fdc29da01442d
Alfredo <alfredodelafuente@avanzosc.es>
2025-06-03 07:44:48 +0200

    [16.0][MIG] sale_order_line_copy: Migration to 16.0

--- sale_order_line_copy/models/sale_order_line.py
+++ sale_order_line_copy/models/sale_order_line.py
@@ -1,12 +1,11 @@
 # Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.multi
     def copy_sale_order_line(self):
         for line in self:
             line.copy(
