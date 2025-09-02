0e36826d075ee2ebbccf16257dda6202e3f56aaa
Ane <aneavanzosc@gmail.com>
2025-09-02 10:35:51 +0200

    [16.0][MIG] sale_required_shipping_address: Migration to 16.0

--- sale_required_shipping_address/models/sale_order.py
+++ sale_required_shipping_address/models/sale_order.py
@@ -1,23 +1,22 @@
 # Copyright 2021 Alfredo de la fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     allowed_shipping_ids = fields.Many2many(
-        string='Allowed shipping', comodel_name='res.partner')
+        string="Allowed shipping", comodel_name="res.partner"
+    )
 
-    @api.multi
-    @api.onchange('partner_id')
-    def onchange_partner_id(self):
-        result = super(SaleOrder, self).onchange_partner_id()
+    @api.onchange("partner_id", "company_id")
+    def _onchange_partner_id(self):
+        result = super()._onchange_partner_id()
         if self.partner_id:
-            partners = self.partner_id
-            if self.partner_id.child_ids:
-                deliveries = self.partner_id.child_ids.filtered(
-                    lambda x: x.type == 'delivery')
+            partners = self.partner_id.commercial_partner_id
+            if partners.child_ids:
+                deliveries = partners.child_ids.filtered(lambda x: x.type == "delivery")
                 for delivery in deliveries:
                     partners += delivery
                 if len(deliveries) == 1:
@@ -28,28 +27,27 @@ class SaleOrder(models.Model):
             self.allowed_shipping_ids = [(6, 0, partners.ids)]
         return result
 
-    @api.onchange('partner_id')
-    def onchange_partner_id_warning(self):
+    def _onchange_partner_id_warning(self):
         if not self.partner_id:
             return
-        warning = super(SaleOrder, self).onchange_partner_id_warning()
+        warning = super()._onchange_partner_id_warning()
         if warning is None:
             warning = {}
         my_warning = {} if warning == bool else warning
         if len(self.allowed_shipping_ids) > 1:
-            message = _('More than one delivery address found ')
+            message = _("More than one delivery address found ")
             if not my_warning:
                 title = _("Warning for %s") % self.partner_id.name
                 warning2 = {
-                    'title': title,
-                    'message': message,
+                    "title": title,
+                    "message": message,
                 }
-                my_warning = {'warning': warning2}
+                my_warning = {"warning": warning2}
             else:
-                origin_warning = my_warning.get('warning')
-                origin_message = origin_warning.get('message')
-                my_message = ('{}\n{}').format(origin_message, message)
-                my_warning['warning']['message'] = my_message
+                origin_warning = my_warning.get("warning")
+                origin_message = origin_warning.get("message")
+                my_message = ("{}\n{}").format(origin_message, message)
+                my_warning["warning"]["message"] = my_message
         if my_warning:
             return my_warning
         else:
--- sale_required_shipping_address/tests/test_sale_required_shipping_address.py
+++ sale_required_shipping_address/tests/test_sale_required_shipping_address.py
@@ -9,35 +9,36 @@ class TestSaleRequiredShippingAddress(common.SavepointCase):
 
     @classmethod
     def setUpClass(cls):
-        super(TestSaleRequiredShippingAddress, cls).setUpClass()
-        cls.sale_model = cls.env['sale.order']
-        cls.partner_model = cls.env['res.partner']
-        vals = {'name': 'Company 1',
-                'company_type': 'company'}
+        super().setUpClass()
+        cls.sale_model = cls.env["sale.order"]
+        cls.partner_model = cls.env["res.partner"]
+        vals = {"name": "Company 1", "company_type": "company"}
         cls.partner = cls.partner_model.create(vals)
-        vals = {'name': 'Company 1 shipping address 1',
-                'company_type': 'person',
-                'type': 'delivery',
-                'parent_id': cls.partner.id}
+        vals = {
+            "name": "Company 1 shipping address 1",
+            "company_type": "person",
+            "type": "delivery",
+            "parent_id": cls.partner.id,
+        }
         cls.shipping1 = cls.partner_model.create(vals)
-        vals = {'name': 'Company 1 shipping address 2',
-                'company_type': 'person',
-                'type': 'delivery',
-                'parent_id': cls.partner.id}
+        vals = {
+            "name": "Company 1 shipping address 2",
+            "company_type": "person",
+            "type": "delivery",
+            "parent_id": cls.partner.id,
+        }
         cls.shipping2 = cls.partner_model.create(vals)
 
     def test_sale_required_shipping_address(self):
-        cond = [('state', '=', 'draft')]
+        cond = [("state", "=", "draft")]
         sale = self.sale_model.search(cond, limit=1)
         sale.partner_id = self.partner.id
         sale.onchange_partner_id()
         self.assertEqual(len(sale.allowed_shipping_ids), 3)
         result = sale.onchange_partner_id_warning()
-        self.assertIn('warning', result)
-        self.partner.write(
-            {'sale_warn': 'warning',
-             'sale_warn_msg': 'bbbbbb'})
+        self.assertIn("warning", result)
+        self.partner.write({"sale_warn": "warning", "sale_warn_msg": "bbbbbb"})
         result = sale.onchange_partner_id_warning()
-        warning = result.get('warning')
-        message = warning.get('message')
-        self.assertIn('bbbbbb', message)
+        warning = result.get("warning")
+        message = warning.get("message")
+        self.assertIn("bbbbbb", message)
