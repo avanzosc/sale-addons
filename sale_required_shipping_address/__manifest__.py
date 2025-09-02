a5607067d0ee23da7896c2cf6c524fea6335060a
Ane <aneavanzosc@gmail.com>
2025-09-02 10:35:51 +0200

    [16.0][MIG] sale_required_shipping_address: Migration to 16.0

--- sale_required_shipping_address/__manifest__.py
+++ sale_required_shipping_address/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Sale Required Shipping Address",
-    "version": "12.0.1.3.0",
+    "version": "16.0.1.3.0",
     "license": "AGPL-3",
     "depends": [
         "sale_management",
     ],
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/sale-addons",
     "category": "Sales",
     "data": [
         "views/sale_order_view.xml",
