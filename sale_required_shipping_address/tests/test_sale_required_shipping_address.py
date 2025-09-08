c456a3cb0f780acaf4526e0d841a47638e1ded74
Ane <aneavanzosc@gmail.com>
2025-09-08 12:24:49 +0200

    Update test to call _onchange_partner_id() instead of onchange_partner_id()

--- sale_required_shipping_address/tests/test_sale_required_shipping_address.py
+++ sale_required_shipping_address/tests/test_sale_required_shipping_address.py
@@ -32,12 +32,12 @@ class TestSaleRequiredShippingAddress(common.SavepointCase):
         cond = [("state", "=", "draft")]
         sale = self.sale_model.search(cond, limit=1)
         sale.partner_id = self.partner.id
-        sale.onchange_partner_id()
+        sale._onchange_partner_id()
         self.assertEqual(len(sale.allowed_shipping_ids), 3)
-        result = sale.onchange_partner_id_warning()
+        result = sale._onchange_partner_id_warning()
         self.assertIn("warning", result)
         self.partner.write({"sale_warn": "warning", "sale_warn_msg": "bbbbbb"})
-        result = sale.onchange_partner_id_warning()
+        result = sale._onchange_partner_id_warning()
         warning = result.get("warning")
         message = warning.get("message")
         self.assertIn("bbbbbb", message)
