568497accf47dd6d3b11d22b4653e7d59203ad1e
luciaavanzosc <luciaavanzosc@gmail.com>
2025-10-22 12:07:00 +0200

    [14.0][IMP] sale_order_type_last_price_info: Update price unit with sale or invoice last price depending on sale order type.

--- sale_order_type_last_price_info/models/sale_order_line.py
+++ sale_order_type_last_price_info/models/sale_order_line.py
@@ -9,39 +9,32 @@ class SaleOrderLine(models.Model):
     @api.onchange("product_id")
     def product_id_change(self):
         result = super().product_id_change()
-        vals = {}
-        if (
-            self.product_id
-            and self.order_id
-            and self.order_id.type_id
-            and (self.order_id.type_id.sale_price_type == "sale")
-        ):
-            vals["price_unit"] = self.sale_last_price_unit
-        if (
-            self.product_id
-            and self.order_id
-            and self.order_id.type_id
-            and (self.order_id.type_id.sale_price_type == "invoice")
-        ):
-            vals["price_unit"] = self.invoice_last_price_unit
-        self.update(vals)
+        self._compute_last_price_unit()
         return result
 
     @api.onchange("product_uom", "product_uom_qty")
     def product_uom_change(self):
         result = super().product_uom_change()
-        if (
-            self.product_id
-            and self.order_id
-            and self.order_id.type_id
-            and (self.order_id.type_id.sale_price_type == "sale")
-        ):
-            self.price_unit = self.sale_last_price_unit
-        if (
-            self.product_id
-            and self.order_id
-            and self.order_id.type_id
-            and (self.order_id.type_id.sale_price_type == "invoice")
-        ):
-            self.price_unit = self.invoice_last_price_unit
+        self._update_price_unit_from_last_price()
         return result
+
+    @api.onchange("sale_last_price_unit", "invoice_last_price_unit")
+    def last_price_change(self):
+        self._update_price_unit_from_last_price()
+
+    def write(self, vals):
+        res = super().write(vals)
+        if any(
+            field in vals
+            for field in ["sale_last_price_unit", "invoice_last_price_unit"]
+        ):
+            self._update_price_unit_from_last_price()
+        return res
+
+    def _update_price_unit_from_last_price(self):
+        for line in self:
+            if line.product_id and line.order_id and line.order_id.type_id:
+                if line.order_id.type_id.sale_price_type == "sale":
+                    line.price_unit = line.sale_last_price_unit
+                elif line.order_id.type_id.sale_price_type == "invoice":
+                    line.price_unit = line.invoice_last_price_unit
