563ce2966b86e4ca74d564d74409e1d630a22971
Ane <aneavanzosc@gmail.com>
2025-11-04 11:42:19 +0100

    [16.0][MIG] sale_order_line_attachment: Migration to 16.0

--- sale_order_line_attachment/models/sale_order_line.py
+++ sale_order_line_attachment/models/sale_order_line.py
@@ -1,16 +1,16 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, api
+from odoo import models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.multi
+
     def action_get_attachment_view(self):
         self.ensure_one()
-        res = self.env['ir.actions.act_window'].for_xml_id(
-            'base', 'action_attachment')
+        res = self.env['ir.actions.actions']._for_xml_id(
+            'base.action_attachment')
         res['domain'] = [('res_model', '=', 'sale.order.line'),
                          ('res_id', 'in', self.ids)]
         res['context'] = {'default_res_model': 'sale.order.line',
