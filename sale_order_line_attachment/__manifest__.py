6f8d1835eb058f875f38f57598efcbaafed3dc04
Ane <aneavanzosc@gmail.com>
2025-11-04 11:46:50 +0100

    [IMP] : pre-commit auto fixes

--- sale_order_line_attachment/__manifest__.py
+++ sale_order_line_attachment/__manifest__.py
@@ -2,14 +2,11 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Sale Order Line Attachment",
-    "version": "16.0.1.1.0",
+    "version": "16.0.1.0.0",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/sale-addons",
     "license": "AGPL-3",
-    "depends": [
-        "sale",
-        "mail"
-    ],
+    "depends": ["sale", "mail"],
     "category": "Sales",
     "data": [
         "wizard/wiz_catch_attachment_from_product_view.xml",
--- sale_order_line_attachment/models/ir_attachment.py
+++ sale_order_line_attachment/models/ir_attachment.py
@@ -1,20 +1,21 @@
 # Copyright 2021 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class IrAttachment(models.Model):
-    _inherit = 'ir.attachment'
+    _inherit = "ir.attachment"
 
     attach_in_sales_orders = fields.Boolean(
-        string='Attach in sales orders', default=True)
-    
-    show_attach_in_sale = fields.Boolean(
-        string= 'Is product attachment',
-        compute = '_compute_show_attach_in_sale',
+        string="Attach in sales orders", default=True
     )
 
-    @api.depends('res_model')
+    show_attach_in_sale = fields.Boolean(
+        string="Is product attachment",
+        compute="_compute_show_attach_in_sale",
+    )
+
+    @api.depends("res_model")
     def _compute_show_attach_in_sale(self):
         for att in self:
-            att.show_attach_in_sale = (att.res_model == 'product.template')
+            att.show_attach_in_sale = att.res_model == "product.template"
