8c5aee4a546648f5c7ba9ce85bff2eaf155bcdc6
Ane <aneavanzosc@gmail.com>
2025-11-04 11:42:19 +0100

    [16.0][MIG] sale_order_line_attachment: Migration to 16.0

--- sale_order_line_attachment/wizard/mail_compose_message.py
+++ sale_order_line_attachment/wizard/mail_compose_message.py
@@ -1,21 +1,21 @@
 # Copyright 2021 Alfredo de la fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, api
+from odoo import models
 
 
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
-    @api.multi
-    def onchange_template_id(self, template_id, composition_mode, model,
+    def _onchange_template_id(self, template_id, composition_mode, model,
                              res_id):
         attachment_obj = self.env['ir.attachment']
-        result = super(MailComposeMessage, self).onchange_template_id(
+        result = super(MailComposeMessage, self)._onchange_template_id(
             template_id, composition_mode, model, res_id)
+        value = (result.get('value') or {})
+        attachment_ids = (value.get('attachment_ids') or [])
+
         if ('active_model' in self.env.context and
                 self.env.context.get('active_model', 'a') == 'sale.order'):
-            value = result.get('value')
-            attachment_ids = value.get('attachment_ids')
             sale = self.env['sale.order'].browse(
                 self.env.context.get('active_id'))
             for line in sale.order_line:
@@ -24,5 +24,6 @@ class MailComposeMessage(models.TransientModel):
                 attachmets = attachment_obj.search(cond)
                 for attachment in attachmets:
                     attachment_ids.append((4, attachment.id))
-            result['value']['attachment_ids'] = attachment_ids
+        value['attachment_ids'] = attachment_ids
+        result['value']= value
         return result
