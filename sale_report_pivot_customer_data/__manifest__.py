cb16817f6db00196d10bd5050315b869969f1f1b
Ane <aneavanzosc@gmail.com>
2025-11-04 13:52:37 +0100

    [18.0][MIG] sale_report_pivot_customer_data: Migration to 18.0

--- sale_report_pivot_customer_data/__manifest__.py
+++ sale_report_pivot_customer_data/__manifest__.py
@@ -2,11 +2,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Spacorp Sales Analysis Report Extension",
-    "version": "16.0.1.2.0",
+    "version": "18.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "https://github.com/avanzosc/sale-addons",
+    "website": "http://www.avanzosc.es",
     "depends": [
         "sale",
     ],
