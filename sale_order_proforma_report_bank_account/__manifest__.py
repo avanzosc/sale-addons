10602f5e9f0c2acade92b4d49dd66440a00acd9e
Alfredo <alfredodelafuente@avanzosc.es>
2025-11-17 13:17:38 +0100

    [16.0][ADD] sale_order_proforma_report_bank_account: In sales orders, when printing a proforma invoice, display bank account details.

---
+++ sale_order_proforma_report_bank_account/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Proforma Report Bank Account",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "depends": ["sale", "account_payment_sale"],
+    "data": [
+        "report/sale_order_report.xml",
+    ],
+    "installable": True,
+}
