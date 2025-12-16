e472b31d1059c32b968937e9944309abfc854eda
Ane <aneavanzosc@gmail.com>
2025-12-16 12:06:17 +0100

    [18.0][FIX] sale_order_line_attachment: include sale order line attachments when sending email

--- sale_order_line_attachment/wizard/mail_compose_message.py
+++ sale_order_line_attachment/wizard/mail_compose_message.py
@@ -6,27 +6,55 @@ from odoo import models
 class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
 
-    def _onchange_template_id(self, template_id, composition_mode, model, res_id):
-        attachment_obj = self.env["ir.attachment"]
-        result = super()._onchange_template_id(
-            template_id, composition_mode, model, res_id
-        )
-        value = result.get("value") or {}
-        attachment_ids = value.get("attachment_ids") or []
+    def _compute_attachment_ids(self):
+        res = super()._compute_attachment_ids()
 
-        if (
-            "active_model" in self.env.context
-            and self.env.context.get("active_model", "a") == "sale.order"
-        ):
-            sale = self.env["sale.order"].browse(self.env.context.get("active_id"))
-            for line in sale.order_line:
-                cond = [
-                    ("res_model", "=", "sale.order.line"),
-                    ("res_id", "in", line.ids),
-                ]
-                attachmets = attachment_obj.search(cond)
-                for attachment in attachmets:
-                    attachment_ids.append((4, attachment.id))
-        value["attachment_ids"] = attachment_ids
-        result["value"] = value
-        return result
+        for composer in self:
+            model = (
+                composer.model
+                or composer.env.context.get("active_model")
+                or composer.env.context.get("default_model")
+            )
+            if model != "sale.order":
+                continue
+
+            res_ids = composer._evaluate_res_ids()
+            if len(res_ids) != 1:
+                continue
+
+            sale = composer.env["sale.order"].browse(res_ids[0])
+            line_ids = sale.order_line.ids
+            if not line_ids:
+                continue
+
+            attaches = (
+                composer.env["ir.attachment"]
+                .sudo()
+                .search(
+                    [
+                        ("res_model", "=", "sale.order.line"),
+                        ("res_id", "in", line_ids),
+                    ]
+                )
+            )
+            if not attaches:
+                continue
+
+            existing_names = set(composer.attachment_ids.mapped("name"))
+            to_add = composer.env["ir.attachment"]
+            for att in attaches:
+                if att.name in existing_names:
+                    continue
+                copied = att.with_user(composer.env.user).copy(
+                    {
+                        "res_model": composer._name,
+                        "res_id": composer.id,
+                    }
+                )
+                to_add |= copied
+                existing_names.add(att.name)
+
+            if to_add:
+                composer.attachment_ids |= to_add
+
+        return res
