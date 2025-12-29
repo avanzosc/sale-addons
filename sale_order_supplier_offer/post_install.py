1f350304a68cbe7dcd9bcc461fcbe252c0412fbc
Ane <aneavanzosc@gmail.com>
2025-12-29 11:44:31 +0100

    [IMP] sale_order_supplier_offer: pre-commit auto fixes

--- sale_order_supplier_offer/post_install.py
+++ sale_order_supplier_offer/post_install.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import SUPERUSER_ID, api
+
 
 def purchase_order_with_sale_order(env):
     sale_orders = env["sale.order"].search([])
