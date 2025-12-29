ff201e64f18493cae1529479ccc1873b4b3cc6f4
Ane <aneavanzosc@gmail.com>
2025-12-29 10:22:34 +0100

    [18.0][MIG] sale_order_product_shortcut: Migration to 18.0

--- sale_order_product_shortcut/__manifest__.py
+++ sale_order_product_shortcut/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/sale-addons",
     "category": "Sales/Sales",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["product", "sale"],
     "data": [
