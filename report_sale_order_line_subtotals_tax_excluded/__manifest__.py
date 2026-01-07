0d4e2b6a347149a9737df6f5a663c6dce9a51b84
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-07 09:04:14 +0100

    [16.0][ADD] report_sale_order_line_subtotals_tax_excluded: In sale order report print amount without tax.

---
+++ report_sale_order_line_subtotals_tax_excluded/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Report Sale Order Line Subtotals Tax Excluded",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "report/sale_order_report.xml",
+    ],
+    "installable": True,
+}
