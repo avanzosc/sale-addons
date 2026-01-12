63bde515b3424e4d1fd45560eca0e50c3ccdaaee
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-12 10:38:58 +0100

    [18.0][MIG] sale_order_type_picking_account: Migration to 18.0

--- sale_order_type_picking_account/models/stock_move.py
+++ sale_order_type_picking_account/models/stock_move.py
@@ -1,14 +1,17 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    def _get_new_picking_values(self):
-        values = super(StockMove, self)._get_new_picking_values()
-        group = self.mapped("group_id")
-        if group and group.sale_id and group.sale_id.type_id:
-            values["sale_type_id"] = group.sale_id.type_id.id
-        return values
+    sale_type_id = fields.Many2one(
+        string="Sale type",
+        comodel_name="sale.order.type",
+        related="picking_id.sale_type_id",
+        store=True,
+        copy=False,
+    )
+
+    picking_type_code = fields.Selection(related="picking_id.picking_type_code")
