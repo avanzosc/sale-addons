f7fc5bb4a4c8d76411ec27d4ad8ecb0bc644bc25
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-27 08:30:55 +0100

    [18.0][ADD] sale_order_line_price_unit_control: When confirming a sales order, verify that none of its lines have a unit price equal to 1.

---
+++ sale_order_line_price_unit_control/tests/test_sale_order_line_price_unit_control.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase
+
+
+class TestSaleOrderLinePriceUnitControl(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cond = [("state", "=", "draft")]
+        cls.sale = cls.env["sale.order"].search(cond, limit=1)
+        cond = [("state", "=", "draft"), ("id", "!=", cls.sale.id)]
+        cls.sale2 = cls.env["sale.order"].search(cond, limit=1)
+
+    def test_sale_order_line_price_unit_control(self):
+        self.sale2.order_line[0].write({"price_unit": 1})
+        self.sale2.action_confirm()
+        self.assertEqual(self.sale2.state, "sale")
+        self.sale.order_line[0].write({"price_unit": 1})
+        with self.assertRaises(UserError):
+            self.sale.with_context(check_amount_less=True).action_confirm()
