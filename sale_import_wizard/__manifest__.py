d49e188fbc3f0057ed29fe8f09413d8f513d697a
Alfredo <alfredodelafuente@avanzosc.es>
2026-02-27 07:38:49 +0100

    [16.0][MIG] sale_import_wizard: Migration to 16.0

--- sale_import_wizard/__manifest__.py
+++ sale_import_wizard/__manifest__.py
@@ -2,17 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Import Wizard",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Hidden/Tools",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/sale-addons",
-    "depends": [
-        "sale",
-        "sales_team",
-        "base_import_wizard",
-        "stock",
-    ],
+    "depends": ["sale", "sales_team", "base_import_wizard", "product_trim_name"],
     "data": [
         "security/ir.model.access.csv",
         "security/sale_import_wizard_security.xml",
