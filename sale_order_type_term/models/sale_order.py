e2136b8b72b3d251adb82191694c315a9f873991
aneravanzosc <aneravanzosc@gmail.com>
2026-03-11 15:25:59 +0100

    [18.0][MIG] sale_order_type_term: Migration to 18.0

--- sale_order_type_term/models/sale_order.py
+++ sale_order_type_term/models/sale_order.py
@@ -7,20 +7,18 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.onchange("type_id")
-    def onchange_type_id(self):
-        result = super(SaleOrder, self).onchange_type_id()
+    def _onchange_type_id_set_note(self):
         for order in self.filtered(lambda x: x.type_id and x.type_id.description):
             order.note = order.type_id.description
-        return result
 
     @api.onchange("partner_id")
-    def onchange_partner_id(self):
+    def _onchange_partner_id(self):
         """
         Overrides the default behavior when changing the partner
         on a sale order to keep the 'note' field unchanged.
         """
         original_note = self.note
-        result = super(SaleOrder, self).onchange_partner_id()
+        result = super()._onchange_partner_id()
         if original_note:
             self.note = original_note
         return result
