1500b4d7356d217393f75e04aa51180abe03f800
aneravanzosc <aneravanzosc@gmail.com>
2026-03-12 09:48:29 +0100

    [18.0][MIG] sale_line_qty_update_in_picking: Migration to 18.0

--- sale_line_qty_update_in_picking/__manifest__.py
+++ sale_line_qty_update_in_picking/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Line Qty Update In Picking",
-    "version": "14.0.1.0.0",
-    "author": "Avanzosc",
+    "version": "18.0.1.0.0",
+    "author": "AvanzOSC",
     "category": "Inventory",
     "website": "https://github.com/avanzosc/sale-addons",
     "depends": [
