c0269883b716d821f8fdd4513d10910a67c868f4
aneravanzosc <aneravanzosc@gmail.com>
2026-03-12 15:54:09 +0100

    [18.0][MIG] sale_purchase_inter_company_origin: Migration to 18.0

--- sale_purchase_inter_company_origin/__manifest__.py
+++ sale_purchase_inter_company_origin/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Purchase Inter Company Origin",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Intercompany SO/PO/INV rules",
     "license": "AGPL-3",
     "author": "AvanzOSC",
