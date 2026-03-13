f04fabfb53b891e608b0a6f77393494bd53ed7bc
aneravanzosc <aneravanzosc@gmail.com>
2026-03-13 10:01:28 +0100

    [18.0][MIG] stock_production_lot_sale_price: Migration to 18.0

--- stock_production_lot_sale_price/models/stock_production_lot.py
+++ stock_production_lot_sale_price/models/stock_production_lot.py
@@ -4,13 +4,15 @@ from odoo import fields, models
 
 
 class StockProductionLot(models.Model):
-    _inherit = "stock.production.lot"
+    _inherit = "stock.lot"
 
-    sale_price = fields.Float(string="Sale Price", compute="_compute_sale_price")
+    sale_price = fields.Float(compute="_compute_sale_price")
 
     def _compute_sale_price(self):
         for lot in self:
             price = 0
+            total_amount = 0
+            total_qty = 0
             lines = self.env["stock.move.line"].search(
                 [
                     ("lot_id", "=", lot.id),
@@ -19,5 +21,18 @@ class StockProductionLot(models.Model):
                 ]
             )
             if lines:
-                price = sum(lines.mapped("amount")) / sum(lines.mapped("qty_done"))
+                for line in lines:
+                    qty = line.product_uom_id._compute_quantity(
+                        line.quantity, line.product_id.uom_id
+                    )
+                    if not qty:
+                        continue
+                    if line.move_id.sale_line_id:
+                        unit_price = line.move_id.sale_line_id.price_unit
+                    else:
+                        unit_price = line.product_id.standard_price
+                    total_amount += unit_price * qty
+                    total_qty += qty
+                if total_qty:
+                    price = total_amount / total_qty
             lot.sale_price = price
