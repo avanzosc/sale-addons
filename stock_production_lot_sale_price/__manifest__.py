08f063f1d3de776d2da1655bd359515f2110738e
aneravanzosc <aneravanzosc@gmail.com>
2026-03-13 10:01:28 +0100

    [18.0][MIG] stock_production_lot_sale_price: Migration to 18.0

--- stock_production_lot_sale_price/__manifest__.py
+++ stock_production_lot_sale_price/__manifest__.py
@@ -2,18 +2,18 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Production Lot Sale Price",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Inventory/Inventory",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/sale-addons",
     "depends": [
         "stock",
-        "sale",
-        "stock_move_line_cost",
+        "sale_stock",
     ],
     "data": [
         "views/stock_production_lot_view.xml",
     ],
     "installable": True,
+    "auto_install": False,
 }
