d4e9afccc01289b115da58f14fdf3b958869621c
aneravanzosc <aneravanzosc@gmail.com>
2026-05-12 09:23:42 +0200

    [18.0][MIG] sale_order_type_config: Migration to 18.0

--- sale_order_type_config/models/purchase_order.py
+++ sale_order_type_config/models/purchase_order.py
@@ -10,6 +10,7 @@ class PurchaseOrder(models.Model):
 
     @api.onchange("company_id")
     def _onchange_company_id(self):
-        super(PurchaseOrder, self)._onchange_company_id()
+        result = super()._onchange_company_id()
         if self.picking_type_id:
             self.picking_type_id = False
+        return result
