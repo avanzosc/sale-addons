fc062c02e57522c0e6d453ff3c3346095d8ec730
Ane <aneavanzosc@gmail.com>
2026-05-18 13:27:38 +0200

    [16.0][ADD] website_sale_free_qty_virtual_available:  use forecasted stock in website sales

---
+++ website_sale_free_qty_virtual_available/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2026 Ane Gurruchaga - Avanz0SC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Sale Free Qty Virtual Available",
+    "version": "16.0.1.0.0",
+    "category": "Website",
+    "author": "Avanz0SC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale",
+        "website_sale_stock",
+        "stock",
+    ],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
