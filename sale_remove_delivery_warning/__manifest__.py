2c63399c91c92451e013c7562c1f10d86043efbd
Ane <aneavanzosc@gmail.com>
2026-05-18 09:47:58 +0200

    [16.0][FIX] sale_remove_delivery_warning: Remove delivery date warning on sales

---
+++ sale_remove_delivery_warning/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Ane Gurruchaga - Avanz0SC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Remove Delivery Warning",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+        "sale_stock",
+    ],
+    "data": [],
+    "installable": True,
+}
