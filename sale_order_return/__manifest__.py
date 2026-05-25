e8bd48cec88de2a69be4a717ff1be1185f33bfbb
luciaavanzosc <luciaavanzosc@gmail.com>
2026-05-25 09:34:33 +0200

    [18.0][MIG] sale_order_return: Migration to 18.0.

--- sale_order_return/__manifest__.py
+++ sale_order_return/__manifest__.py
@@ -2,13 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Return",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Avanzosc",
     "category": "Sales",
     "website": "https://github.com/avanzosc/sale-addons",
     "depends": [
+        "purchase_sale_inter_company",
         "sale_order_confirm_usability",
         "sale_order_type_picking",
+        "sale_order_lot_selection",
     ],
     "data": [
         "views/sale_order_view.xml",
