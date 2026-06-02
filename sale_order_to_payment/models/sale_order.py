0ec421b7069ee16860f977004b5266a7e41c41f2
aneravanzosc <aneravanzosc@gmail.com>
2026-06-02 13:15:59 +0200

    [18.0][MIG] sale_order_to_payment: Migration to 18.0

--- sale_order_to_payment/models/sale_order.py
+++ sale_order_to_payment/models/sale_order.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -15,28 +15,27 @@ class SaleOrder(models.Model):
         string="Payments Count", compute="_compute_payment_count"
     )
 
+    @api.depends("payment_ids")
     def _compute_payment_count(self):
         for sale in self:
             sale.payment_count = len(sale.payment_ids)
 
+    @api.depends("invoice_ids.reconciled_payment_ids")
     def _compute_payment_ids(self):
         for sale in self:
-            sale.payment_ids = False
-            payments = []
+            payments = self.env["account.payment"]
             for move in sale.invoice_ids:
-                for payment in move.payment_ids:
-                    if payment not in payments:
-                        payments.append(payment.id)
-            if payments:
-                sale.payment_ids = [(6, 0, payments)]
+                payments |= move.reconciled_payment_ids
+            sale.payment_ids = payments
 
     def action_view_payments(self):
+        self.ensure_one()
         context = self.env.context.copy()
         if self.partner_id:
             context.update({"default_partner_id": self.partner_id.id})
         return {
             "name": _("Payments"),
-            "view_mode": "tree,form",
+            "view_mode": "list,form",
             "res_model": "account.payment",
             "domain": [("id", "in", self.payment_ids.ids)],
             "type": "ir.actions.act_window",
