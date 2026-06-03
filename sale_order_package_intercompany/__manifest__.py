28aefe8baac3e1a8dff27ee7a607712363567531
Alfredo <alfredodelafuente@avanzosc.es>
2026-06-03 14:18:34 +0200

    [18.0][MIG] sale_order_package_intercompany: Migration to 18.0

--- sale_order_package_intercompany/__manifest__.py
+++ sale_order_package_intercompany/__manifest__.py
@@ -2,14 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Order Package Inter Company",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/sale-addons",
     "category": "Sale",
     "depends": [
         "sale_order_line_qty_by_packaging",
-        "purchase_order_line_packaging_qty",
+        "purchase_stock_packaging",
         "custom_saca_intercompany",
+        "purchase_sale_inter_company",
     ],
     "data": [],
     "license": "AGPL-3",
