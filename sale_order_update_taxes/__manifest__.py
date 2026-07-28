2114db4adcb9d224b7274e55f7044285a1824b34
Ane <aneavanzosc@gmail.com>
2026-07-28 13:50:58 +0200

    [18.0][ADD] sale_order_update_taxes: show update taxes button on confirmed orders

---
+++ sale_order_update_taxes/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Ane Gurruchaga - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Update Taxes",
+    "version": "18.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
