ae86c4651e0ab39337cc44e83c26902dd8cbba3c
Lucía Echeverría <luciaavanzosc@gmail.com>
2026-08-06 10:31:22 +0200

    [18.0][ADD] sale_order_force_no_invoice: Allow forcing invoice status to "Nothing to Invoice"

---
+++ sale_order_force_no_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 AvanzOSC - Lucía Echevarría
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Force No Invoice",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
