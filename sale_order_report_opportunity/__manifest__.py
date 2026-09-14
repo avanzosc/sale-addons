503bc92d42ec31c11448610aed30633a91821b3a
luciaavanzosc <luciaavanzosc@gmail.com>
2026-09-14 11:35:35 +0200

    [18.0][ADD] sale_order_report_opportunity: Show the opportunity of the quotation in the sale order report

---
+++ sale_order_report_opportunity/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 AvanzOSC - Lucía Echeverría
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Order Report Opportunity",
+    "version": "18.0.1.0.0",
+    "summary": "Show the opportunity of the quotation in the sale order report",
+    "category": "Sales/Sales",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_crm",
+    ],
+    "data": [
+        "report/ir_actions_report_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
