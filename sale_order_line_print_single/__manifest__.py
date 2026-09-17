bfc5b9067c89b25fa7b0e8681b01c193c57657c9
Alfredo <alfredodelafuente@avanzosc.es>
2026-09-17 12:00:14 +0200

    [18.0][ADD] sale_order_line_print_single: When printing sales orders, print the sales order line on a single line.

---
+++ sale_order_line_print_single/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Print Single",
+    "version": "18.0.1.0.0",
+    "category": "Sales/Sales",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/sale-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "reports/sale_order_reports.xml",
+    ],
+    "installable": True,
+}
